61693a86d5e64431043ad94a25c62460dd201d52
Pranav K <prkrishn@hotmail.com>
2014-02-27 23:20:28 -0800

    Reintroduce model validation

---
+++ src/Microsoft.AspNet.Mvc.ModelBinding/Validation/ModelValidatingEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Microsoft.AspNet.Mvc.ModelBinding
+{
+    public sealed class ModelValidatingEventArgs : CancelEventArgs
+    {
+        public ModelValidatingEventArgs(ModelValidationContext validationContext, ModelValidationNode parentNode)
+        {
+            ValidationContext = validationContext;
+            ParentNode = parentNode;
+        }
+
+        public ModelValidationContext ValidationContext { get; private set; }
+
+        public ModelValidationNode ParentNode { get; private set; }
+    }
+}
