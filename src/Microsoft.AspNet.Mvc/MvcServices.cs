d47f5fdf6b563b487d912a117847e5850587b78c
Ryan Nowak <nowakra@gmail.com>
2014-03-10 17:44:29 -0700

    Fix for issue #57
    Replaced ActivatorUtilties with ITypeActivator in WebFX.

--- src/Microsoft.AspNet.Mvc/MvcServices.cs
+++ src/Microsoft.AspNet.Mvc/MvcServices.cs
@@ -15,6 +15,8 @@ namespace Microsoft.AspNet.Mvc
         {
             var describe = new ServiceDescriber(configuration);
 
+            yield return describe.Transient<ITypeActivator, TypeActivator>();
+
             yield return describe.Transient<IControllerFactory, DefaultControllerFactory>();
             yield return describe.Transient<IControllerDescriptorFactory, DefaultControllerDescriptorFactory>();
             yield return describe.Transient<IActionSelector, DefaultActionSelector>();
