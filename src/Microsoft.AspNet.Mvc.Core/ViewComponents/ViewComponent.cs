a8d31ed349c5395dcd67a8de5d3217ea66cbac9e
Ryan Nowak <nowakra@gmail.com>
2014-03-12 18:41:58 -0700

    Prototype of components

---
+++ src/Microsoft.AspNet.Mvc.Core/ViewComponents/ViewComponent.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Microsoft.AspNet.Mvc
+{
+    public class ViewComponent
+    {
+        public IViewComponentResultHelper Result { get; private set; }
+
+        public ViewData ViewData { get; set; }
+
+        public void Initialize(IViewComponentResultHelper result)
+        {
+            Result = result;
+        }
+    }
+}
