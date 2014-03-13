39adcd0c683572192e727beabc77cbf6ae8ae0fd
Ryan Nowak <nowakra@gmail.com>
2014-03-12 18:41:58 -0700

    Prototype of components

---
+++ src/Microsoft.AspNet.Mvc.Core/ViewComponents/IViewComponentResultHelper.cs
@@ -0,0 +1,12 @@
+﻿
+namespace Microsoft.AspNet.Mvc
+{
+    public interface IViewComponentResultHelper
+    {
+        IViewComponentResult Content(string content);
+
+        IViewComponentResult Json(object value);
+
+        IViewComponentResult View(string viewName, ViewData viewData);
+    }
+}
