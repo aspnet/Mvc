719d6118c3329d9f7e5f711528fef6c2fabc93fe
dougbu <dougbu@microsoft.com>
2014-04-10 12:53:50 -0700

    Merge extension files by concept - Display / DisplayFor / DisplayForModel all into Display - Partial / RenderPartial all into Partial

--- src/Microsoft.AspNet.Mvc.Core/Rendering/HtmlHelperDisplayExtensions.cs
+++ src/Microsoft.AspNet.Mvc.Core/Rendering/HtmlHelperDisplayExtensions.cs
@@ -1,4 +1,6 @@
-﻿
+﻿using System;
+using System.Linq.Expressions;
+
 namespace Microsoft.AspNet.Mvc.Rendering
 {
     public static class HtmlHelperDisplayExtensions
@@ -38,6 +40,78 @@ namespace Microsoft.AspNet.Mvc.Rendering
                                                  string htmlFieldName)
         {
             return html.Display(expression, templateName, htmlFieldName, additionalViewData: null);
+        }
+
+        public static HtmlString DisplayFor<TModel, TValue>([NotNull] this IHtmlHelper<TModel> html,
+                                                            [NotNull] Expression<Func<TModel, TValue>> expression)
+        {
+            return html.DisplayFor<TValue>(expression, templateName: null, htmlFieldName: null,
+                additionalViewData: null);
+        }
+
+        public static HtmlString DisplayFor<TModel, TValue>([NotNull] this IHtmlHelper<TModel> html,
+                                                            [NotNull] Expression<Func<TModel, TValue>> expression,
+                                                            object additionalViewData)
+        {
+            return html.DisplayFor<TValue>(expression, templateName: null, htmlFieldName: null,
+                additionalViewData: additionalViewData);
+        }
+
+        public static HtmlString DisplayFor<TModel, TValue>([NotNull] this IHtmlHelper<TModel> html,
+                                                            [NotNull] Expression<Func<TModel, TValue>> expression,
+                                                            string templateName)
+        {
+            return html.DisplayFor<TValue>(expression, templateName, htmlFieldName: null, additionalViewData: null);
+        }
+
+        public static HtmlString DisplayFor<TModel, TValue>([NotNull] this IHtmlHelper<TModel> html,
+                                                            [NotNull] Expression<Func<TModel, TValue>> expression,
+                                                            string templateName,
+                                                            object additionalViewData)
+        {
+            return html.DisplayFor<TValue>(expression, templateName, htmlFieldName: null,
+                additionalViewData: additionalViewData);
+        }
+
+        public static HtmlString DisplayFor<TModel, TValue>([NotNull] this IHtmlHelper<TModel> html,
+                                                            [NotNull] Expression<Func<TModel, TValue>> expression,
+                                                            string templateName,
+                                                            string htmlFieldName)
+        {
+            return html.DisplayFor<TValue>(expression, templateName: templateName, htmlFieldName: htmlFieldName,
+                additionalViewData: null);
+        }
+
+        public static HtmlString DisplayForModel<TModel>([NotNull] this IHtmlHelper<TModel> html)
+        {
+            return html.DisplayForModel(templateName: null, htmlFieldName: null, additionalViewData: null);
+        }
+
+        public static HtmlString DisplayForModel<TModel>([NotNull] this IHtmlHelper<TModel> html,
+                                                         object additionalViewData)
+        {
+            return html.DisplayForModel(templateName: null, htmlFieldName: null,
+                additionalViewData: additionalViewData);
+        }
+
+        public static HtmlString DisplayForModel<TModel>([NotNull] this IHtmlHelper<TModel> html,
+                                                         string templateName)
+        {
+            return html.DisplayForModel(templateName, htmlFieldName: null, additionalViewData: null);
+        }
+
+        public static HtmlString DisplayForModel<TModel>([NotNull] this IHtmlHelper<TModel> html,
+                                                         string templateName,
+                                                         object additionalViewData)
+        {
+            return html.DisplayForModel(templateName, htmlFieldName: null, additionalViewData: additionalViewData);
+        }
+
+        public static HtmlString DisplayForModel<TModel>([NotNull] this IHtmlHelper<TModel> html,
+                                                         string templateName,
+                                                         string htmlFieldName)
+        {
+            return html.DisplayForModel(templateName, htmlFieldName, additionalViewData: null);
         }
     }
 }
