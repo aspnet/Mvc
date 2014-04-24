27fccf0fcaad4d480939390ed663b4e48620ff3c
Ryan Nowak <nowakra@gmail.com>
2014-04-24 16:32:08 -0700

    Bringing back UrlHelper.IsLocalUrl
    This has been compied verbatim from MVC (intentional). The tests have been
    modernized a bit as well, but all the cases covered in the original are
    there.
    
    This may be moved to HttpAbstractions at some point in the future.

--- src/Microsoft.AspNet.Mvc.Core/UrlHelper.cs
+++ src/Microsoft.AspNet.Mvc.Core/UrlHelper.cs
@@ -67,6 +67,13 @@ namespace Microsoft.AspNet.Mvc
             return GenerateUrl(protocol, host, path, fragment);
         }
 
+        public bool IsLocalUrl(string url)
+        {
+            return !string.IsNullOrEmpty(url) &&
+                   ((url[0] == '/' && (url.Length == 1 || (url[1] != '/' && url[1] != '\\'))) || // "/" or "/foo" but not "//" or "/\"
+                    (url.Length > 1 && url[0] == '~' && url[1] == '/')); // "~/" or "~/foo"
+        }
+
         public string RouteUrl(object values, string protocol, string host, string fragment)
         {
             var valuesDictionary = TypeHelper.ObjectToDictionary(values);
