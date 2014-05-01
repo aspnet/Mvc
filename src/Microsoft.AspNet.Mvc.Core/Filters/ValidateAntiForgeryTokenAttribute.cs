f5335797305ec32fa12ca2ee3c6508ff48330666
harshgMSFT <harshg@microsoft.com>
2014-05-01 10:20:48 -0700

    Adding ValidateAntiForgeryTokenAttribute also fixing a bug in token deserialization.

---
+++ src/Microsoft.AspNet.Mvc.Core/Filters/ValidateAntiForgeryTokenAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNet.DependencyInjection;
+
+namespace Microsoft.AspNet.Mvc
+{
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = false, Inherited = true)]
+    public class ValidateAntiForgeryTokenAttribute : Attribute, IFilterFactory
+    {
+        public IFilter CreateInstance(IServiceProvider serviceProvider)
+        {
+            var antiForgeryInstance = serviceProvider.GetService<AntiForgery>();
+            return new ValidateAntiForgeryTokenAuthorizationFilter(antiForgeryInstance);
+        }
+    }
+}
---
+++ src/Microsoft.AspNet.Mvc.Core/Filters/ValidateAntiForgeryTokenAuthorizationFilter.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.Mvc
+{
+    public class ValidateAntiForgeryTokenAuthorizationFilter : IAsyncAuthorizationFilter, IOrderedFilter
+    {
+        private AntiForgery _antiForgeryInstance;
+
+        public int Order { get; private set; }
+
+        public ValidateAntiForgeryTokenAuthorizationFilter([NotNull] AntiForgery antiForgeryInstance)
+        {
+            _antiForgeryInstance = antiForgeryInstance;
+            Order = -1;
+        }
+
+        public async Task OnAuthorizationAsync([NotNull] AuthorizationContext context)
+        {
+            await _antiForgeryInstance.ValidateAsync(context.HttpContext);
+        }
+    }
+}
