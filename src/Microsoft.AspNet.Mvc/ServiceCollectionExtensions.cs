4da56cc2eeea60344707819b89b035f28bc286e5
Pranav K <prkrishn@hotmail.com>
2014-05-06 12:53:26 -0700

    Reacting to package and namespace changes

--- src/Microsoft.AspNet.Mvc/ServiceCollectionExtensions.cs
+++ src/Microsoft.AspNet.Mvc/ServiceCollectionExtensions.cs
@@ -16,7 +16,7 @@
 // permissions and limitations under the License.
 
 using Microsoft.Framework.ConfigurationModel;
-using Microsoft.AspNet.DependencyInjection;
+using Microsoft.Framework.DependencyInjection;
 
 namespace Microsoft.AspNet.Mvc
 {
