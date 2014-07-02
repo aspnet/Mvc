965904d4b57611108e7d597fa08b4a7238634a33
sornaks <sornaks@microsoft.com>
2014-07-01 17:11:49 -0700

    Introducing input XML Formatters.

---
+++ src/Microsoft.AspNet.Mvc.ModelBinding/Formatters/DelegatingStream.cs
@@ -0,0 +1,162 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.Mvc.ModelBinding
+{
+    /// <summary>
+    /// Stream that delegates to inner stream.
+    /// </summary>
+    internal class DelegatingStream : Stream
+    {
+        private Stream _innerStream;
+
+        internal DelegatingStream(Stream innerStream)
+        {
+            if (innerStream == null)
+            {
+                throw new ArgumentNullException("innerStream");
+            }
+            _innerStream = innerStream;
+        }
+
+        protected Stream InnerStream
+        {
+            get { return _innerStream; }
+        }
+
+        public override bool CanRead
+        {
+            get { return _innerStream.CanRead; }
+        }
+
+        public override bool CanSeek
+        {
+            get { return _innerStream.CanSeek; }
+        }
+
+        public override bool CanWrite
+        {
+            get { return _innerStream.CanWrite; }
+        }
+
+        public override long Length
+        {
+            get { return _innerStream.Length; }
+        }
+
+        public override long Position
+        {
+            get { return _innerStream.Position; }
+            set { _innerStream.Position = value; }
+        }
+
+        public override int ReadTimeout
+        {
+            get { return _innerStream.ReadTimeout; }
+            set { _innerStream.ReadTimeout = value; }
+        }
+
+        public override bool CanTimeout
+        {
+            get { return _innerStream.CanTimeout; }
+        }
+
+        public override int WriteTimeout
+        {
+            get { return _innerStream.WriteTimeout; }
+            set { _innerStream.WriteTimeout = value; }
+        }
+
+        public override long Seek(long offset, SeekOrigin origin)
+        {
+            return _innerStream.Seek(offset, origin);
+        }
+
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return _innerStream.Read(buffer, offset, count);
+        }
+
+        public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            return _innerStream.ReadAsync(buffer, offset, count, cancellationToken);
+        }
+#if NETFX_CORE
+        public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+        {
+            return _innerStream.BeginRead(buffer, offset, count, callback, state);
+        }
+
+        public override int EndRead(IAsyncResult asyncResult)
+        {
+            return _innerStream.EndRead(asyncResult);
+        }
+#endif
+
+        public override int ReadByte()
+        {
+            return _innerStream.ReadByte();
+        }
+
+        public override void Flush()
+        {
+            _innerStream.Flush();
+        }
+
+        public override Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken)
+        {
+            return _innerStream.CopyToAsync(destination, bufferSize, cancellationToken);
+        }
+
+        public override Task FlushAsync(CancellationToken cancellationToken)
+        {
+            return _innerStream.FlushAsync(cancellationToken);
+        }
+
+        public override void SetLength(long value)
+        {
+            _innerStream.SetLength(value);
+        }
+
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            _innerStream.Write(buffer, offset, count);
+        }
+
+        public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            return _innerStream.WriteAsync(buffer, offset, count, cancellationToken);
+        }
+#if NETFX_CORE
+        public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback callback, object state)
+        {
+            return _innerStream.BeginWrite(buffer, offset, count, callback, state);
+        }
+
+        public override void EndWrite(IAsyncResult asyncResult)
+        {
+            _innerStream.EndWrite(asyncResult);
+        }
+#endif
+
+        public override void WriteByte(byte value)
+        {
+            _innerStream.WriteByte(value);
+        }
+#if NETFX_CORE
+        public override void Close()
+        {
+        }
+#endif
+        protected override void Dispose(bool disposing)
+        {
+            // No-op. In CoreCLR this is equivalent to Close. 
+            // Given that we don't own the underlying stream, we never want to do anything interesting here.
+        }
+    }
+}
---
+++ src/Microsoft.AspNet.Mvc.ModelBinding/Formatters/XmlSerializerInputFormatter.cs
@@ -0,0 +1,113 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Serialization;
+
+namespace Microsoft.AspNet.Mvc.ModelBinding
+{
+    public class XmlSerializerInputFormatter : IInputFormatter
+    {
+        private IList<Encoding> _supportedEncodings;
+        private IList<string> _supportedMediaTypes;
+        private XmlDictionaryReaderQuotas _readerQuotas = FormattingUtilities.GetDefaultReaderQuotas();
+
+        public XmlSerializerInputFormatter()
+        {
+            _supportedMediaTypes = new List<string>
+            {
+                "application/xml",
+                "text/xml"
+            };
+
+            _supportedEncodings = new List<Encoding>
+            {
+                new UTF8Encoding(encoderShouldEmitUTF8Identifier: false, throwOnInvalidBytes: true),
+                new UnicodeEncoding(bigEndian: false, byteOrderMark: true, throwOnInvalidBytes: true)
+            };
+        }
+
+        public IList<Encoding> SupportedEncodings
+        {
+            get { return _supportedEncodings; }
+        }
+
+        public IList<string> SupportedMediaTypes
+        {
+            get { return _supportedMediaTypes; }
+        }
+
+        public int MaxDepth
+        {
+            get
+            {
+                return _readerQuotas.MaxDepth;
+            }
+            set
+            {
+                if (value < FormattingUtilities.DefaultMinDepth)
+                {
+                    throw new ArgumentOutOfRangeException("value");
+                }
+
+                _readerQuotas.MaxDepth = value;
+            }
+        }
+
+        public async Task ReadAsync(InputFormatterContext context)
+        {
+            var request = context.HttpContext.Request;
+            if (request.ContentLength == 0)
+            {
+                var modelType = context.Metadata.ModelType;
+                context.Model = modelType.GetTypeInfo().IsValueType ? Activator.CreateInstance(modelType) :
+                                                                      null;
+                return;
+            }
+
+            var effectiveEncoding = FormattingUtilities.SelectCharacterEncoding(SupportedEncodings,
+                request.GetContentType(), typeof(XmlSerializerInputFormatter));
+            context.Model = await ReadInternal(context, effectiveEncoding);
+        }
+
+        /// <summary>
+        /// Called during deserialization to get the <see cref="XmlReader"/>.
+        /// </summary>
+        /// <param name="readStream">The <see cref="Stream"/> from which to read.</param>
+        /// <param name="effectiveEncoding">The <see cref="Encoding"/> to use when reading.</param>
+        /// <returns>The <see cref="XmlReader"/> used during deserialization.</returns>
+        public virtual XmlReader CreateXmlReader([NotNull] Stream readStream,
+                                                 [NotNull] Encoding effectiveEncoding)
+        {
+            return XmlDictionaryReader.CreateTextReader(
+                new DelegatingStream(readStream), effectiveEncoding, _readerQuotas, null);
+        }
+
+        /// <summary>
+        /// Called during deserialization to get the <see cref="XmlSerializer"/>.
+        /// </summary>
+        /// <returns>The <see cref="XmlSerializer"/> used during serialization and deserialization.</returns>
+        public virtual XmlSerializer CreateXmlSerializer(Type type)
+        {
+            return new XmlSerializer(type);
+        }
+
+        private Task<object> ReadInternal(InputFormatterContext context, Encoding effectiveEncoding)
+        {
+            var type = context.Metadata.ModelType;
+            var request = context.HttpContext.Request;
+
+            using (var xmlReader = CreateXmlReader(request.Body, effectiveEncoding))
+            {
+                var xmlSerializer = CreateXmlSerializer(type);
+                return Task.FromResult(xmlSerializer.Deserialize(xmlReader));
+            }
+        }
+    }
+}
