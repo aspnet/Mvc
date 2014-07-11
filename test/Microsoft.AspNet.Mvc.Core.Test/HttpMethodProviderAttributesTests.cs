76fb77e1d978bbead41ee2c602ded136d84c1648
Javier Calvarro Nelson <jacalvar@microsoft.com>
2014-07-10 19:37:36 -0700

    [Issue #730] Attribute Routing: Flesh out attributes - Part 2
    1. Unsealed the Http*Attributes so that they can be extended and customized.
    2. Added the same constructors as HttpGet to the rest of the Http*Attributes.
    3. Added unit tests to validate the implementations for the IActionHttpMethodProvider.
    4. Added functional tests to cover extra attribute routing scenarios like a test for an
       action with an HttpDeleteAttribute on it and action with AcceptVerbsAttribute and an
       action with a custom HttpMergeAttribute implemented.

---
+++ test/Microsoft.AspNet.Mvc.Core.Test/HttpMethodProviderAttributesTests.cs
@@ -0,0 +1,35 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc.Core.Test
+{
+    public class HttpMethodProviderAttributesTests
+    {
+        [Theory]
+        [MemberData("HttpMethodProviderTestData")]
+        public void HttpConstraints_Return(IActionHttpMethodProvider httpMethodProvider, IEnumerable<string> expectedHttpMethods)
+        {
+            // Act & Assert
+            Assert.Equal(expectedHttpMethods, httpMethodProvider.HttpMethods);
+        }
+
+        public static TheoryData<IActionHttpMethodProvider, IEnumerable<string>> HttpMethodProviderTestData
+        {
+            get
+            {
+                var data = new TheoryData<IActionHttpMethodProvider, IEnumerable<string>>();
+                data.Add(new HttpGetAttribute(), new[] { "GET" });
+                data.Add(new HttpPostAttribute(), new[] { "POST" });
+                data.Add(new HttpPutAttribute(), new[] { "PUT" });
+                data.Add(new HttpPatchAttribute(), new[] { "PATCH" });
+                data.Add(new HttpDeleteAttribute(), new[] { "DELETE" });
+                data.Add(new AcceptVerbsAttribute("MERGE", "OPTIONS"), new[] { "MERGE", "OPTIONS" });
+
+                return data;
+            }
+        }
+    }
+}
