dfd59ba3247f5be24bc8a1017afdb45842fa9e14
harshgMSFT <harshg@microsoft.com>
2014-07-15 17:51:24 -0700

    Http Abstractions for parsing Http headers needed for conneg

---
+++ src/Microsoft.AspNet.Mvc.HeaderValueAbstractions/MediaTypeHeaderValue.cs
@@ -0,0 +1,78 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.AspNet.Mvc.HeaderValueAbstractions
+{
+    public class MediaTypeHeaderValue
+    {
+        public string Charset { get; set; }
+
+        public string MediaType { get; set; }
+
+        public string MediaSubType { get; set; }
+
+        public string RawValue { get; set; }
+
+        public IDictionary<string, string> Parameters { get; set; }
+
+        public static MediaTypeHeaderValue Parse(string input)
+        {
+            if(string.IsNullOrEmpty(input))
+            {
+                return null;
+            }
+
+            var inputArray = input.Split(new[] { ';' }, 2);
+            var mediaTypeParts = inputArray[0].Split('/');
+            if(mediaTypeParts.Length != 2)
+            {
+                return null;
+            }
+
+            // TODO: throw if the media type and subtypes are invalid.
+            var mediaType = mediaTypeParts[0].Trim();
+            var mediaSubType = mediaTypeParts[1].Trim();
+
+            Dictionary<string, string> parameters = null;
+            string charset = null;
+            if (inputArray.Length == 2)
+            {
+                parameters = ParseParameters(inputArray[1]);
+                if (parameters.ContainsKey("charset"))
+                {
+                    charset = parameters["charset"];
+                }
+            }
+
+            var mediaTypeHeader = new MediaTypeHeaderValue()
+            {
+                MediaType = mediaType,
+                MediaSubType = mediaSubType,
+                Charset = charset,
+                Parameters = parameters,
+                RawValue = input
+            };
+
+            return mediaTypeHeader;
+        }
+
+        protected static Dictionary<string, string> ParseParameters(string inputString)
+        {
+            var acceptParameters = inputString.Split(';');
+            var parameterNameValue = new Dictionary<string, string>();
+            foreach (var parameter in acceptParameters)
+            {
+                var index = parameter.Split('=');
+                if (index.Length == 2)
+                {
+                    // TODO: throw exception if this is not the case.
+                    parameterNameValue.Add(index[0].Trim(), index[1].Trim());
+                }
+            }
+
+            return parameterNameValue;
+        }
+    }
+}
---
+++ src/Microsoft.AspNet.Mvc.HeaderValueAbstractions/MediaTypeWithQualityHeaderValue.cs
@@ -0,0 +1,51 @@
+// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+
+namespace Microsoft.AspNet.Mvc.HeaderValueAbstractions
+{
+    public class MediaTypeWithQualityHeaderValue : MediaTypeHeaderValue
+    {
+        public double? Quality { get; set; }
+
+        public IDictionary<string, string> AcceptParameters { get; set; }
+
+        public static new MediaTypeWithQualityHeaderValue Parse(string input)
+        {
+            var mediaTypeString = input;
+            var quality = FormattingUtilities.Match;
+            Dictionary<string, string> acceptParameters = null;
+
+            // TODO: this does not take care if there was a space between.
+            var index = input.IndexOf("q=", System.StringComparison.Ordinal);
+            if (index > 0)
+            {
+                var acceptParameterString = input.Substring(index);
+                acceptParameters = ParseParameters(acceptParameterString);
+                mediaTypeString = input.Substring(0, index);
+                quality = Double.Parse(acceptParameters["q"]);
+            }
+
+            var mediaTypeHeaderValue = MediaTypeHeaderValue.Parse(mediaTypeString);
+            if(mediaTypeHeaderValue == null)
+            {
+                return null;
+            }
+
+            return
+                new MediaTypeWithQualityHeaderValue()
+                {
+                    MediaType = mediaTypeHeaderValue.MediaType,
+                    MediaSubType = mediaTypeHeaderValue.MediaSubType,
+                    Charset = mediaTypeHeaderValue.Charset,
+                    RawValue = input,
+                    Parameters = mediaTypeHeaderValue.Parameters,
+                    Quality = quality,
+                    AcceptParameters = acceptParameters
+                };
+        } 
+       
+    }
+}
