ec71e57402bd081d2ec84fe0ad4eb354173faa3a
sornaks <sornaks@microsoft.com>
2014-10-05 19:08:21 -0700

    Issue #937: Adding functional tests for Filters.

---
+++ test/WebSites/FiltersWebSite/Controllers/FakeUserController.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc;
+
+namespace FiltersWebSite
+{
+    [FakeUser]
+    public class FakeUserController : Controller
+    {
+        [Authorize("Permission", "CanViewPage")]
+        public string ReturnHelloWorldOnlyForFakeUser()
+        {
+            return "Hello World!";
+        }
+    }
+}
