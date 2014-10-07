04093fe99636ddf1ed64ad18e2d4ed9056605e1b
sornaks <sornaks@microsoft.com>
2014-10-06 17:54:40 -0700

    Fixing line lengths.

--- test/Microsoft.AspNet.Mvc.ModelBinding.Test/Validation/DefaultBodyModelValidatorTests.cs
+++ test/Microsoft.AspNet.Mvc.ModelBinding.Test/Validation/DefaultBodyModelValidatorTests.cs
@@ -35,111 +35,168 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
                 yield return new object[] { "foo", typeof(string), new Dictionary<string, string>() };
 
                 // Object Traversal : make sure we can traverse the object graph without throwing
-                yield return new object[] { new ValueType() { Reference = "ref", Value = 256 }, typeof(ValueType), new Dictionary<string, string>() };
-                yield return new object[] { new ReferenceType() { Reference = "ref", Value = 256 }, typeof(ReferenceType), new Dictionary<string, string>() };
+                yield return new object[]
+                {
+                    new ValueType() { Reference = "ref", Value = 256 },
+                    typeof(ValueType),
+                    new Dictionary<string, string>()
+                };
+                yield return new object[]
+                {
+                    new ReferenceType() { Reference = "ref", Value = 256 },
+                    typeof(ReferenceType),
+                    new Dictionary<string, string>()
+                };
 
                 // Classes
-                yield return new object[] { new Person() { Name = "Rick", Profession = "Astronaut" }, typeof(Person), new Dictionary<string, string>() };
-                yield return new object[] { new Person(), typeof(Person), new Dictionary<string, string>()
-                        {
-                            { "Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
-                            { "Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") }
-                        }
-                    };
+                yield return new object[]
+                {
+                    new Person() { Name = "Rick", Profession = "Astronaut" },
+                    typeof(Person),
+                    new Dictionary<string, string>()
+                };
+                yield return new object[]
+                {
+                    new Person(),
+                    typeof(Person),
+                    new Dictionary<string, string>()
+                    {
+                        { "Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
+                        { "Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") }
+                    }
+                };
 
-                yield return new object[] { new Person() { Name = "Rick", Friend = new Person() }, typeof(Person), new Dictionary<string, string>()
-                        {
-                            { "Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") },
-                            { "Friend.Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
-                            { "Friend.Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") }
-                        }
-                    };
+                yield return new object[]
+                {
+                    new Person() { Name = "Rick", Friend = new Person() },
+                    typeof(Person),
+                    new Dictionary<string, string>()
+                    {
+                        { "Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") },
+                        { "Friend.Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
+                        { "Friend.Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") }
+                    }
+                };
 
                 // Collections
-                yield return new object[] { new Person[] { new Person(), new Person() }, typeof(Person[]), new Dictionary<string, string>()
-                        {
-                            { "[0].Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
-                            { "[0].Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") },
-                            { "[1].Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
-                            { "[1].Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") }
-                        }
-                    };
+                yield return new object[]
+                {
+                    new Person[] { new Person(), new Person() },
+                    typeof(Person[]),
+                    new Dictionary<string, string>()
+                    {
+                        { "[0].Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
+                        { "[0].Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") },
+                        { "[1].Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
+                        { "[1].Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") }
+                    }
+                };
 
-                yield return new object[] { new List<Person> { new Person(), new Person() }, typeof(Person[]), new Dictionary<string, string>()
-                        {
-                            { "[0].Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
-                            { "[0].Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") },
-                            { "[1].Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
-                            { "[1].Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") }
-                        }
-                    };
+                yield return new object[]
+                {
+                    new List<Person> { new Person(), new Person() },
+                    typeof(Person[]),
+                    new Dictionary<string, string>()
+                    {
+                        { "[0].Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
+                        { "[0].Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") },
+                        { "[1].Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
+                        { "[1].Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") }
+                    }
+                };
 
-                yield return new object[] { new Dictionary<string, Person> { { "Joe", new Person() } , { "Mark", new Person() } }, typeof(Dictionary<string, Person>), new Dictionary<string, string>()
-                        {
-                            { "[0].Value.Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
-                            { "[0].Value.Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") },
-                            { "[1].Value.Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
-                            { "[1].Value.Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") }
-                        }
-                    };
+                yield return new object[]
+                {
+                    new Dictionary<string, Person> { { "Joe", new Person() } , { "Mark", new Person() } },
+                    typeof(Dictionary<string, Person>),
+                    new Dictionary<string, string>()
+                    {
+                        { "[0].Value.Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
+                        { "[0].Value.Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") },
+                        { "[1].Value.Name", ValidationAttributeUtil.GetRequiredErrorMessage("Name") },
+                        { "[1].Value.Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") }
+                    }
+                };
 
                 // IValidatableObject's
-                yield return new object[] { new ValidatableModel(), typeof(ValidatableModel), new Dictionary<string, string>()
-                        {
-                            { "", "Error1" },
-                            { "Property1", "Error2" },
-                            { "Property2", "Error3" },
-                            { "Property3", "Error3" }
-                        }
-                    };
+                yield return new object[]
+                {
+                    new ValidatableModel(),
+                    typeof(ValidatableModel),
+                    new Dictionary<string, string>()
+                    {
+                        { "", "Error1" },
+                        { "Property1", "Error2" },
+                        { "Property2", "Error3" },
+                        { "Property3", "Error3" }
+                    }
+                };
 
-                yield return new object[]{ new[] { new ValidatableModel() }, typeof(ValidatableModel[]), new Dictionary<string, string>()
-                        {
-                            { "[0]", "Error1" },
-                            { "[0].Property1", "Error2" },
-                            { "[0].Property2", "Error3" },
-                            { "[0].Property3", "Error3" }
-                        }
-                    };
+                yield return new object[]
+                {
+                    new[] { new ValidatableModel() },
+                    typeof(ValidatableModel[]),
+                    new Dictionary<string, string>()
+                    {
+                        { "[0]", "Error1" },
+                        { "[0].Property1", "Error2" },
+                        { "[0].Property2", "Error3" },
+                        { "[0].Property3", "Error3" }
+                    }
+                };
 
                 // Nested Objects
-                yield return new object[] { new Org() {
-                            Id = 1,
-                            OrgName = "Org",
-                            Dev = new Team
-                            {
-                                Id = 10,
-                                TeamName = "HelloWorldTeam",
-                                Lead = "SampleLeadDev",
-                                TeamSize = 2
-                            },
-                            Test = new Team
-                            {
-                                Id = 11,
-                                TeamName = "HWT",
-                                Lead = "SampleTestLead",
-                                TeamSize = 12
-                            }
-                        }, typeof(Org), new Dictionary<string, string>()
-                            {
-                                { "OrgName", ValidationAttributeUtil.GetStringLengthErrorMessage(4, 20, "OrgName") },
-                                { "Dev.Lead", ValidationAttributeUtil.GetMaxLengthErrorMessage(10, "Lead") },
-                                { "Dev.TeamSize", ValidationAttributeUtil.GetRangeErrorMessage(3, 100, "TeamSize") },
-                                { "Test.TeamName", ValidationAttributeUtil.GetStringLengthErrorMessage(4, 20, "TeamName") },
-                                { "Test.Lead", ValidationAttributeUtil.GetMaxLengthErrorMessage(10, "Lead") }
-                            }
+                yield return new object[]
+                {
+                    new Org()
+                    {
+                        Id = 1,
+                        OrgName = "Org",
+                        Dev = new Team
+                        {
+                            Id = 10,
+                            TeamName = "HelloWorldTeam",
+                            Lead = "SampleLeadDev",
+                            TeamSize = 2
+                        },
+                        Test = new Team
+                        {
+                            Id = 11,
+                            TeamName = "HWT",
+                            Lead = "SampleTestLead",
+                            TeamSize = 12
+                        }
+                    },
+                    typeof(Org),
+                    new Dictionary<string, string>()
+                    {
+                        { "OrgName", ValidationAttributeUtil.GetStringLengthErrorMessage(4, 20, "OrgName") },
+                        { "Dev.Lead", ValidationAttributeUtil.GetMaxLengthErrorMessage(10, "Lead") },
+                        { "Dev.TeamSize", ValidationAttributeUtil.GetRangeErrorMessage(3, 100, "TeamSize") },
+                        { "Test.TeamName", ValidationAttributeUtil.GetStringLengthErrorMessage(4, 20, "TeamName") },
+                        { "Test.Lead", ValidationAttributeUtil.GetMaxLengthErrorMessage(10, "Lead") }
+                    }
                 };
 
                 // Testing we don't validate fields
-                yield return new object[] { new VariableTest() { test = 5 }, typeof(VariableTest), new Dictionary<string, string>() };
+                yield return new object[]
+                {
+                    new VariableTest() { test = 5 },
+                    typeof(VariableTest),
+                    new Dictionary<string, string>()
+                };
 
                 // Testing we don't blow up on cycles
-                yield return new object[] { LonelyPerson, typeof(Person), new Dictionary<string, string>()
-                        {
-                            { "Name", ValidationAttributeUtil.GetStringLengthErrorMessage(null, 10, "Name") },
-                            { "Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") }
-                        }
-                    };
+                yield return new object[]
+                {
+                    LonelyPerson,
+                    typeof(Person),
+                    new Dictionary<string, string>()
+                    {
+                        { "Name", ValidationAttributeUtil.GetStringLengthErrorMessage(null, 10, "Name") },
+                        { "Profession", ValidationAttributeUtil.GetRequiredErrorMessage("Profession") }
+                    }
+                };
             }
         }
 
