73101f1ac3ec8e078dbc7ddf3a4c7dec2c71109f
NTaylorMullen <nimullen@microsoft.com>
2014-10-07 20:50:20 -0700

    Add ModelExpression code generation.
    - Sealed the ModelExpression.
    - We use the stringified version of the ModelExpression type name to detect ModelExpression properties on TagHelpers. This is so the MvcRazorHost can work in tooling and in runtime.
    - Created a GeneratedTagHelperAttributeContext to represent the specific stringified versions of the ModelExpression assets.
    - Created an MvcTagHelperAttributeValueCodeRenderer to modify rendering of ModelExpression properties.
    
    #1241

---
+++ src/Microsoft.AspNet.Mvc.Razor.Host/GeneratedTagHelperAttributeContext.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Razor.Runtime.TagHelpers;
+
+namespace Microsoft.AspNet.Mvc.Razor
+{
+    /// <summary>
+    /// Contains information for the <see cref="ITagHelper"/> attribute code generation process.
+    /// </summary>
+    public class GeneratedTagHelperAttributeContext
+    {
+        /// <summary>
+        /// String representation of the model expression type.
+        /// </summary>
+        public string ModelExpressionTypeName { get; set; }
+
+        /// <summary>
+        /// String representation of a method to create model expression types.
+        /// </summary>
+        public string CreateModelExpressionMethodName { get; set; }
+    }
+}
