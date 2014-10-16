d73f381063fe57c57008590daa3534f2b7a4dc0d
Pranav K <prkrishn@hotmail.com>
2014-10-16 11:18:10 -0700

    ViewComponent.Invoke() should be able to invoke views Fixes #285

--- src/Microsoft.AspNet.Mvc.Core/ViewComponents/IViewComponentResult.cs
+++ src/Microsoft.AspNet.Mvc.Core/ViewComponents/IViewComponentResult.cs
@@ -5,10 +5,22 @@ using System.Threading.Tasks;
 
 namespace Microsoft.AspNet.Mvc
 {
+    /// <summary>
+    /// Specifies the contract for the result of a <see cref="ViewComponent"/>.
+    /// </summary>
     public interface IViewComponentResult
     {
+        /// <summary>
+        /// Locates and renders a view component specified by <paramref name="context"/>.
+        /// </summary>
+        /// <param name="context">The <see cref="ViewComponentContext"/> for the current component execution.</param>
         void Execute([NotNull] ViewComponentContext context);
 
+        /// <summary>
+        /// Asynchronously locates and renders a view component specified by <paramref name="context"/>.
+        /// </summary>
+        /// <param name="context">The <see cref="ViewComponentContext"/> for the current component execution.</param>
+        /// <returns>A <see cref="Task"/> that represents the asynchronous execution.</returns>
         Task ExecuteAsync([NotNull] ViewComponentContext context);
     }
 }
