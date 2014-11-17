07696c80285a8d3524436b5147f311b5dc0ede63
SonjaKhan <sonjak3@uw.edu>
2014-11-17 10:52:33 -0800

    logging assemblies, controllers, and actions

--- src/Microsoft.AspNet.Mvc.Core/Logging/LogFormatter.cs
+++ src/Microsoft.AspNet.Mvc.Core/Logging/LogFormatter.cs
@@ -2,6 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Framework.Logging;
 
 namespace Microsoft.AspNet.Mvc.Logging
 {
@@ -32,6 +35,54 @@ namespace Microsoft.AspNet.Mvc.Logging
             }
 
             return "";
+        }
+
+        /// <summary>
+        /// Formats an <see cref="ILoggerStructure"/>.
+        /// </summary>
+        /// <param name="structure">The <see cref="ILoggerStructure"/> to format.</param>
+        /// <returns>A string representation of the given <see cref="ILoggerStructure"/>.</returns>
+        public static string FormatStructure(ILoggerStructure structure)
+        {
+            var values = structure.GetValues();
+            if (values == null)
+            {
+                return string.Empty;
+            }
+            var builder = new StringBuilder();
+            foreach (var kvp in values)
+            {
+                builder.Append(kvp.Key);
+                builder.Append(": ");
+                if ((kvp.Value as IEnumerable<ILoggerStructure>) != null)
+                {
+                    var valArray = ((IEnumerable<ILoggerStructure>)kvp.Value).AsArray();
+                    for (int j = 0; j < valArray.Length - 1; j++)
+                    {
+                        builder.Append(valArray[j].Format());
+                        builder.Append(", ");
+                    }
+                    if (valArray.Length > 0)
+                    {
+                        builder.Append(valArray[valArray.Length - 1].Format());
+                    }
+                }
+                else if ((kvp.Value as ILoggerStructure) != null)
+                {
+                    builder.Append(((ILoggerStructure)kvp.Value).Format());
+                }
+                else
+                {
+                    builder.Append(kvp.Value);
+                }
+                builder.Append("  ");
+            }
+            // get rid of the extra whitespace
+            if (builder.Length > 0)
+            {
+                builder.Length -= 2;
+            }
+            return builder.ToString();
         }
     }
 }
---
+++ test/Microsoft.AspNet.Mvc.Core.Test/ActionModelValuesTest.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc.ApplicationModels;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc.Logging
+{
+    public class ActionModelValuesTest
+    {
+        [Fact]
+        public void ActionModelValues_IncludesAllProperties()
+        {
+            // Arrange
+            string[] exclude = { "Controller", "Attributes", "IsActionNameMatchRequired" };
+
+            // Assert
+            PropertiesHelper.AssertPropertiesAreTheSame(
+                typeof(ActionModel), 
+                typeof(ActionModelValues), 
+                exclude);
+        }
+    }
+}
