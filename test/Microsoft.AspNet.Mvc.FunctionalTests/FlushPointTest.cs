40559c1c68cec87d831109c8dde74d245f3add56
Kirthi Krishnamraju <kirthik@microsoft.com>
2014-12-12 17:05:41 -0800

    Added more coverage in functional tests for FlushAsync

--- test/Microsoft.AspNet.Mvc.FunctionalTests/FlushPointTest.cs
+++ test/Microsoft.AspNet.Mvc.FunctionalTests/FlushPointTest.cs
@@ -63,7 +63,20 @@ namespace Microsoft.AspNet.Mvc.FunctionalTests
             waitService.WaitForServer();
 
             // Assert - 3
-            Assert.Equal("Final content", GetTrimmedString(stream));
+            Assert.Equal(String.Join("", "Inside partial", ""), GetTrimmedString(stream));
+            waitService.WaitForServer();
+                
+            // Assert - 4
+            Assert.Equal(string.Join(
+                Environment.NewLine,
+                "After flush inside partial",
+                "<form action=\"/FlushPoint/PageWithoutLayout\" method=\"post\"><input id=\"Name1\" name=\"Name1\" type=\"text\" value=\"\" />"),
+            GetTrimmedString(stream));
+            waitService.WaitForServer();
+
+            // Assert - 5
+            Assert.Equal(@"<input id=""Name2"" name=""Name2"" type=""text"" value="""" /></form>",
+                        GetTrimmedString(stream));
         }
 
         [Theory]
@@ -105,6 +118,58 @@ namespace Microsoft.AspNet.Mvc.FunctionalTests
                 "More content from layout"), GetTrimmedString(stream));
         }
 
+        [Fact]
+        public async Task FlushPointsNestedLayout()
+        {
+            var waitService = new WaitService();
+            var serviceProvider = GetServiceProvider(waitService);
+
+            var server = TestServer.Create(serviceProvider, _app);
+            var client = server.CreateClient();
+
+            // Act
+            var stream = await client.GetStreamAsync("http://localhost/FlushPoint/PageWithNestedLayout");
+
+            // Assert - 1
+            Assert.Equal(@"Inside Nested Layout
+
+<title>Nested Page With Layout</title>",
+                GetTrimmedString(stream));
+            waitService.WaitForServer();
+
+            // Assert - 2
+            Assert.Equal("<span>Nested content that takes time to produce</span>", GetTrimmedString(stream));
+        }
+
+        [Fact]
+        public async Task FlushBeforeCallingLayout()
+        {
+            var waitService = new WaitService();
+            var serviceProvider = GetServiceProvider(waitService);
+
+            var server = TestServer.Create(serviceProvider, _app);
+            var client = server.CreateClient();
+
+             var expectedMessage = "A layout page cannot be rendered after 'FlushAsync' has been invoked.";
+
+            // Act
+            var stream = await client.GetStreamAsync("http://localhost/FlushPoint/PageWithFlushBeforeLayout");
+
+            // Assert - 1
+            Assert.Equal("Initial content", GetTrimmedString(stream));
+            waitService.WaitForServer();
+
+            //Assert - 2
+            try
+            {
+                GetTrimmedString(stream);
+            }
+            catch (Exception ex)
+            {
+                Assert.Equal(expectedMessage, ex.InnerException.Message);
+            }
+        }
+
         private IServiceProvider GetServiceProvider(WaitService waitService)
         {
             var services = new ServiceCollection();
@@ -116,7 +181,8 @@ namespace Microsoft.AspNet.Mvc.FunctionalTests
         {
             var buffer = new byte[1024];
             var count = stream.Read(buffer, 0, buffer.Length);
-            return Encoding.UTF8.GetString(buffer, 0, count).Trim();
+            string content = Encoding.UTF8.GetString(buffer, 0, count).Trim();
+            return content;
         }
     }
 }
