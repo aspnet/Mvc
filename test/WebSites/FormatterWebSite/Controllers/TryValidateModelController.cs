5485d7679cc578455b87231aaa87b7f3d2c82a1e
Kirthi Krishnamraju <kirthik@microsoft.com>
2015-01-14 11:25:14 -0800

    Added Functional Tests for TryValidateModel

---
+++ test/WebSites/FormatterWebSite/Controllers/TryValidateModelController.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc;
+
+namespace FormatterWebSite
+{
+    public class TryValidateModelController : Controller
+    {
+        [HttpGet]
+        public IActionResult GetInvalidUser()
+        {
+            var user = new User()
+            {
+                Id = 0,
+                Name = "x"
+            };
+            if(!TryValidateModel(user))
+            {
+                return Content(ModelState["Id"].Errors[0].ErrorMessage + "," +
+                    ModelState["Name"].Errors[0].ErrorMessage);
+            }
+
+            return Content(string.Empty);
+        }
+
+        [HttpGet]
+        public IActionResult GetInvalidAdminWithPrefix()
+        {
+            var admin = new Administrator()
+            {
+                Id = 1,
+                Name = "John Doe",
+                Designation = "Administrator",
+                AdminAccessCode = 0
+            };
+            if (!TryValidateModel(admin,"admin"))
+            {
+                return Content(ModelState["admin"].Errors[0].ErrorMessage);
+            }
+
+            return Content(string.Empty);
+        }
+
+        [HttpGet]
+        public IActionResult GetValidAdminWithPrefix()
+        {
+            var admin = new Administrator()
+            {
+                Id = 1,
+                Name = "John Doe",
+                Designation = "Administrator",
+                AdminAccessCode = 1
+            };
+            if (!TryValidateModel(admin, "admin"))
+            {
+                return Content("Error validating admin");
+            }
+
+            return Content("Admin user created successfully");
+        }
+    }
+}
---
+++ test/WebSites/FormatterWebSite/Models/AdminValidator.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace FormatterWebSite
+{
+    public class AdminValidator : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var objectType = value.GetType();
+
+            var neededProperties =
+              objectType.GetProperties()
+              .Where(propertyInfo => propertyInfo.Name == "Designation" || propertyInfo.Name == "AdminAccessCode")
+              .ToArray();
+
+            if (neededProperties.Count() != 2)
+            {
+                return new ValidationResult("Could not find Designation and AdminAccessCode properties");
+            }
+
+            var adminAccessCode = Convert.ToInt32(neededProperties[0].GetValue(value, null));
+            var designation = Convert.ToString(neededProperties[1].GetValue(value, null));
+
+            if (string.IsNullOrEmpty(designation) || !designation.Equals("Administrator"))
+            {
+                return new ValidationResult("Designation property does not have the right value");
+            }
+
+            if (adminAccessCode != 1)
+            {
+                return new ValidationResult ("AdminAccessCode property does not have the right value");
+            }
+
+            return null;
+        }
+    }
+}
