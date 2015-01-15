321851892f5e8c2409f29930503cfdf6e2cf4b8a
Ryan Nowak <nowakra@gmail.com>
2015-01-14 18:35:20 -0800

    Fix for #384 - And some other changes to controller as filter
    This is a major change to how we handle the scenario where a controller is
    a filter. We want to change the lifetime of the controller object, by
    scoping it around action filters and result filters. This means that a
    controller class can only implement action filters and result filters.
    
    To implement #384 - we're creating a delegating filter class
    'ControllerFilter' which will forward calls to the implementation of the
    controller. This is discovered in the controller model and added to the
    filter collection. This filter is removable as an opt-out of this feature.
    
    The ControllerFilter only implements action filter and result filter, so
    the new restriction about filter types on Controller is in place. A future
    change will move the instantiation of the controller to after resource
    filters.

---
+++ src/Microsoft.AspNet.Mvc.Core/Filters/ControllerFilter.cs
@@ -0,0 +1,82 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc.Core;
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNet.Mvc.Filters
+{
+    /// <summary>
+    /// A filter implementation which delegates to the controller for supported filter
+    /// interfaces.
+    /// </summary>
+    public class ControllerFilter : IAsyncActionFilter, IAsyncResultFilter, IOrderedFilter
+    {
+        // Controller-filter methods run farthest from the action by default.
+        /// <inheritdoc />
+        public int Order { get; set; } = int.MinValue;
+
+        /// <inheritdoc />
+        public async Task OnActionExecutionAsync([NotNull] ActionExecutingContext context, [NotNull] ActionExecutionDelegate next)
+        {
+            var controller = context.Controller;
+            if (controller == null)
+            {
+                throw new InvalidOperationException(Resources.FormatPropertyOfTypeCannotBeNull(
+                    nameof(context.Controller),
+                    nameof(ActionExecutingContext)));
+            }
+
+            IAsyncActionFilter asyncResultFilter;
+            IActionFilter resultFilter;
+            if ((asyncResultFilter = controller as IAsyncActionFilter) != null)
+            {
+                await asyncResultFilter.OnActionExecutionAsync(context, next);
+            }
+            else if ((resultFilter = controller as IActionFilter) != null)
+            {
+                resultFilter.OnActionExecuting(context);
+                if (context.Result == null)
+                {
+                    resultFilter.OnActionExecuted(await next());
+                }
+            }
+            else
+            {
+                await next();
+            }
+        }
+
+        /// <inheritdoc />
+        public async Task OnResultExecutionAsync([NotNull] ResultExecutingContext context, [NotNull] ResultExecutionDelegate next)
+        {
+            var controller = context.Controller;
+            if (controller == null)
+            {
+                throw new InvalidOperationException(Resources.FormatPropertyOfTypeCannotBeNull(
+                    nameof(context.Controller),
+                    nameof(ResultExecutingContext)));
+            }
+
+            IAsyncResultFilter asyncResultFilter;
+            IResultFilter resultFilter;
+            if ((asyncResultFilter = controller as IAsyncResultFilter) != null)
+            {
+                await asyncResultFilter.OnResultExecutionAsync(context, next);
+            }
+            else if ((resultFilter = controller as IResultFilter) != null)
+            {
+                resultFilter.OnResultExecuting(context);
+                if (!context.Cancel)
+                {
+                    resultFilter.OnResultExecuted(await next());
+                }
+            }
+            else
+            {
+                await next();
+            }
+        }
+    }
+}
