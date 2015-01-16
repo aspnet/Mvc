6fbfeaa464f860bcded6d66fa4a1b7b71f828560
Kirthi Krishnamraju <kirthik@microsoft.com>
2015-01-15 16:57:51 -0800

    Added functionaltests for ModelMetadataType attribute

---
+++ test/WebSites/ModelBindingWebSite/Models/CompanyNameAttribute.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ModelBindingWebSite
+{
+    public class CompanyNameAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            object instance = validationContext.ObjectInstance;
+            var property = instance.GetType().GetProperty("CompanyName");
+            var companyName = property.GetValue(instance);
+
+            if (companyName == null)
+            {
+                return new ValidationResult("CompanyName cannot be null");
+            }
+            return null;
+        }
+    }
+}
---
+++ test/WebSites/ModelBindingWebSite/Models/ProductValidatorAttibute.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace ModelBindingWebSite
+{
+    public class ProductValidatorAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var objectType = value.GetType();
+
+            var neededProperties =
+              objectType.GetProperties()
+              .Where(propertyInfo => propertyInfo.Name == "Name" || propertyInfo.Name == "Country")
+              .ToArray();
+
+            if (neededProperties.Count() != 2)
+            {
+                return new ValidationResult("Could not find Name or Country properties");
+            }
+
+            var name = Convert.ToString(neededProperties.FirstOrDefault(p => p.Name == "Name").GetValue(value, null));
+            var country = Convert.ToString(neededProperties.FirstOrDefault(p => p.Name == "Country").GetValue(value, null));
+
+            if (!country.Equals("USA"))
+            {
+                return new ValidationResult("Country property does not have the right value");
+            }
+
+            if (string.IsNullOrEmpty(name))
+            {
+                return new ValidationResult("Name property cannot be empty");
+            }
+
+            return null;
+        }
+    }
+}
