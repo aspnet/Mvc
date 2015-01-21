665d1426ec29efdb5338579c5fb04a1e21bb758b
Kirthi Krishnamraju <kirthik@microsoft.com>
2015-01-21 11:24:57 -0800

    Moved To contents to validationwebSite

--- test/Microsoft.AspNet.Mvc.FunctionalTests/TryValidateModelTest.cs
+++ test/Microsoft.AspNet.Mvc.FunctionalTests/TryValidateModelTest.cs
@@ -4,6 +4,8 @@
 using System;
 using System.Collections.Generic;
 using System.Net;
+using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.TestHost;
@@ -14,27 +16,33 @@ namespace Microsoft.AspNet.Mvc.FunctionalTests
 {
     public class TryValidateModelTest
     {
-        private readonly IServiceProvider _services = TestHelper.CreateServices(nameof(FormatterWebSite));
-        private readonly Action<IApplicationBuilder> _app = new FormatterWebSite.Startup().Configure;
+        private readonly IServiceProvider _services = TestHelper.CreateServices(nameof(ValidationWebSite));
+        private readonly Action<IApplicationBuilder> _app = new ValidationWebSite.Startup().Configure;
 
         [Fact]
-        public async Task TryValidateModel_SimpleModelInvalidProperties()
+        public async Task TryValidateModel_ProductViewModelInvalidProperties()
         {
             // Arrange
             var server = TestServer.Create(_services, _app);
             var client = server.CreateClient();
+            var input = "{ \"Price\": 2, \"ProductDetails\": {\"Field1\": \"f1\"}}";
+            var content = new StringContent(input, Encoding.UTF8, "application/json");
+            var url =
+                "http://localhost/ModelMetadataTypeValidation/TryValidateModelProductViewModelWithErrorInParameter/0";
 
             // Act
-            var response = await client.GetAsync("http://localhost/TryValidateModel/GetInvalidUser");
+            var response = await client.PostAsync(url, content);
 
             // Assert
-            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
-
-            var json = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(
-                await response.Content.ReadAsStringAsync());
-            Assert.Equal("The field Id must be between 1 and 2000.", json["Id"][0]);
-            Assert.Equal(
-                "The field Name must be a string or array type with a minimum length of '5'.", json["Name"][0]);
+            var body = await response.Content.ReadAsStringAsync();
+            var json = JsonConvert.DeserializeObject<Dictionary<string, string>>(body);
+            Assert.Equal(6, json.Count);
+            Assert.Equal("CompanyName cannot be null", json["product.CompanyName"]);
+            Assert.Equal("The field Price must be between 20 and 100.", json["product.Price"]);
+            Assert.Equal("The Category field is required.", json["product.Category"]);
+            Assert.Equal("The ContactUs field is required.", json["product.Contact"]);
+            Assert.Equal("The Field2 field is required.", json["product.ProductDetails.Field2"]);
+            Assert.Equal("The Field3 field is required.", json["product.ProductDetails.Field3"]);
         }
 
         [Fact]
@@ -43,16 +51,17 @@ namespace Microsoft.AspNet.Mvc.FunctionalTests
             // Arrange
             var server = TestServer.Create(_services, _app);
             var client = server.CreateClient();
+            var url =
+                "http://localhost/ModelMetadataTypeValidation/TryValidateModelSoftwareViewModelNoPrefix";
 
             // Act
-            var response = await client.GetAsync("http://localhost/TryValidateModel/GetInvalidAdminWithPrefix");
+            var response = await client.GetAsync(url);
 
             // Assert
-            Assert.Equal(HttpStatusCode.BadRequest, response.StatusCode);
-
-            var json = JsonConvert.DeserializeObject<Dictionary<string, string[]>>(
-                await response.Content.ReadAsStringAsync());
-            Assert.Equal("AdminAccessCode property does not have the right value", json["admin"][0]);
+            var body = await response.Content.ReadAsStringAsync();
+            var json = JsonConvert.DeserializeObject<Dictionary<string, string>>(body);
+            Assert.Equal(1, json.Count);
+            Assert.Equal("Country property does not have the right value", json["software"]);
         }
 
         [Fact]
@@ -61,9 +70,11 @@ namespace Microsoft.AspNet.Mvc.FunctionalTests
             // Arrange
             var server = TestServer.Create(_services, _app);
             var client = server.CreateClient();
+            var url =
+                "http://localhost/ModelMetadataTypeValidation/TryValidateModelValidModelNoPrefix";
 
             // Act
-            var response = await client.GetAsync("http://localhost/TryValidateModel/GetValidAdminWithPrefix");
+            var response = await client.GetAsync(url);
 
             // Assert
             Assert.Equal(HttpStatusCode.OK, response.StatusCode);
---
+++ test/WebSites/ValidationWebSite/ProductValidatorAttibute.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using ValidationWebSite.Models;
+
+namespace ValidationWebSite
+{
+    public class ProductValidatorAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var product = (Product)value;
+
+            if (!product.Country.Equals("USA") && string.IsNullOrEmpty(product.Name))
+            {
+                return new ValidationResult("Country and Name fields don't have the right values");
+            }
+
+            return null;
+        }
+    }
+}
