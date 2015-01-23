5ce4456eda3dec71e44fefc8476b8fa397eb218c
damianedwards <damian@damianedwards.com>
2015-01-22 18:41:56 -0800

    Use grunt to minify the LinkTagHelper JS before build: - #1580

---
+++ src/Microsoft.AspNet.Mvc.TagHelpers/Gruntfile.js
@@ -0,0 +1,21 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+        uglify: {
+            scripts: {
+                files: [{
+                    expand: true,
+                    cwd: "js",
+                    src: "**/*.js",
+                    dest: "Compiler/Resources"
+                }]
+            }
+        }
+    });
+
+    grunt.loadNpmTasks("grunt-contrib-uglify");
+
+    grunt.registerTask("default", [ "uglify" ]);
+};
---
+++ src/Microsoft.AspNet.Mvc.TagHelpers/js/LinkTagHelper_FallbackJavaScript.js
@@ -0,0 +1,10 @@
+﻿(function (cssTestPropertyName, cssTestPropertyValue, fallbackHref) {
+    // This function finds the previous element (assumed to be meta) and tests it current CSS style using the passed
+    // values, to determine if a stylesheet was loaded. If not, it loads the fallback stylesheet via document.write.
+    var scriptElements = document.getElementsByTagName("SCRIPT"),
+        meta = scriptElements[scriptElements.length - 1].previousElementSibling;
+
+    if (document.defaultView.getComputedStyle(meta)[cssTestPropertyName] !== cssTestPropertyValue) {
+        document.write('\u003clink rel="stylesheet" href="' + fallbackHref + '"/\u003e');
+    }
+})("[[[0]]]", "[[[1]]]", "[[[2]]]");
