d64f960291c7a27b366ac83c72ca905fbc3d22c1
damianedwards <damian@damianedwards.com>
2015-02-02 16:28:32 -0800

    Sorted: - #1580

--- samples/TagHelperSample.Web/Startup.cs
+++ samples/TagHelperSample.Web/Startup.cs
@@ -1,12 +1,12 @@
 ﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using Microsoft.AspNet.Builder;
 using Microsoft.AspNet.Mvc;
 using Microsoft.Framework.DependencyInjection;
 using Microsoft.Framework.Logging;
 using Microsoft.Framework.Logging.Console;
-using System;
 using TagHelperSample.Web.Services;
 
 namespace TagHelperSample.Web
