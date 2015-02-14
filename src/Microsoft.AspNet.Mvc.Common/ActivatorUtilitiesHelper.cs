27471b38a06d19a703c8062b47d0023854f7f17f
Kirthi Krishnamraju <kirthik@microsoft.com>
2015-02-13 17:05:59 -0800

    Added doc for new helper class

--- src/Microsoft.AspNet.Mvc.Common/ActivatorUtilitiesHelper.cs
+++ src/Microsoft.AspNet.Mvc.Common/ActivatorUtilitiesHelper.cs
@@ -1,8 +1,14 @@
-﻿using Microsoft.Framework.DependencyInjection;
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Framework.DependencyInjection;
 using System;
 
 namespace Microsoft.AspNet.Mvc
 {
+    /// <summary>
+    /// Helper class to create delegate for ActivatorUtilities CreateFactory.
+    /// </summary>
     internal static class ActivatorUtilitiesHelper
     {
         public static Func<Type, ObjectFactory> CreateFactory =
