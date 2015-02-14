649a656e070148e6e954c98147500a319d5c4817
Kirthi Krishnamraju <kirthik@microsoft.com>
2015-02-13 16:28:00 -0800

    Added helper

---
+++ src/Microsoft.AspNet.Mvc.Common/ActivatorUtilitiesHelper.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Framework.DependencyInjection;
+using System;
+
+namespace Microsoft.AspNet.Mvc
+{
+    internal static class ActivatorUtilitiesHelper
+    {
+        public static Func<Type, ObjectFactory> CreateFactory =
+            (t) => ActivatorUtilities.CreateFactory(t, Type.EmptyTypes);
+    }
+}
