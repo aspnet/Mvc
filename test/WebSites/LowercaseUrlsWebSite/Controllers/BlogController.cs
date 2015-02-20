b1cb1f81adc9742458c5c2038e0167e5837ee518
Mugdha Kulkarni <mughdak@microsoft.com>
2015-02-19 17:20:13 -0800

    Adding the test cases for generating lower case URLs

---
+++ test/WebSites/LowercaseUrlsWebSite/Controllers/BlogController.cs
@@ -0,0 +1,25 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc;
+
+namespace LowercaseUrlsWebSite
+{
+    public class LowercaseUrls_BlogController : Controller
+    {
+        public string ShowPosts()
+        {
+            return Url.Action();
+        }
+
+        public string Edit(string postName)
+        {
+            return Url.Action();
+        }
+
+        public string GenerateLink()
+        {
+            return Url.Action("GetEmployee", "LowercaseUrls_Employee", new { Name = "MaryKae" , LastName = "McDonald"});
+        }
+    }
+}
