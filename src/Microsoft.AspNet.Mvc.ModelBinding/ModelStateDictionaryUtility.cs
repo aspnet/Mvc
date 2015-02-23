74d4f6b297f8a5d34fb00eb23471b00371011046
Kirthi Krishnamraju <kirthik@microsoft.com>
2015-02-23 10:16:06 -0800

    Added MSD utility

---
+++ src/Microsoft.AspNet.Mvc.ModelBinding/ModelStateDictionaryUtility.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) Microsoft Open Technologies, Inc. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.AspNet.Mvc.ModelBinding
+{
+    /// <summary>
+    /// This class is a utility for ModelStateDictionary
+    /// </summary>
+    public static class ModelStateDictionaryUtility
+    {
+        /// <summary>
+        /// Clears ModelStateDictionary.
+        /// </summary>
+        /// <param name="modelState">The <see cref="ModelStateDictionary"/>.</param>
+        public static void ClearModelStateDictionary(ModelStateDictionary modelState)
+        {
+            foreach(var entry in modelState)
+            {
+                entry.Value.Errors.Clear();
+                entry.Value.ValidationState = ModelValidationState.Unvalidated;
+            }
+        }
+    }
+}
