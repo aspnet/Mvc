e70c0d13b6b323140ea234fe3d8bb9db2ede0d98
damianedwards <damian@damianedwards.com>
2015-03-05 10:28:43 -0800

    Added unit test for TagHelperOptionsCollectionExtensions: - #1689

---
+++ test/Microsoft.AspNet.Mvc.TagHelpers.Test/TagHelperOptionsCollectionExtensionsTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Microsoft.Framework.ConfigurationModel;
+using Microsoft.Framework.DependencyInjection;
+using Microsoft.Framework.OptionsModel;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc.TagHelpers.Test
+{
+    public class TagHelperOptionsCollectionExtensionsTest
+    {
+        [Fact]
+        public void ConfigureForm_GetsOptionsFromConfigurationCorrectly()
+        {
+            // Arrange
+            var config = new Configuration();
+            config.Set("FormTagHelperOptions:GenerateAntiForgeryToken", "true");
+            var services = new ServiceCollection();
+
+            // Act
+            services.ConfigureTagHelpers()
+                .ConfigureForm(config);
+            var options = services.BuildServiceProvider()
+                .GetService<IOptions<FormTagHelperOptions>>()
+                .Options;
+
+            // Assert
+            Assert.NotNull(options.GenerateAntiForgeryToken);
+            Assert.True(options.GenerateAntiForgeryToken.Value);
+        }
+    }
+}
