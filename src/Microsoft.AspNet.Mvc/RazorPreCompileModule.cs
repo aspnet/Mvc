84089876d746add3054865bdd7159c213c117c5b
Doug Bunting <dougbu@microsoft.com>
2015-03-10 16:49:59 -0700

    Remove fallback `IServiceProvider` implementation in `RazorPreCompileModule` - general code cleanup and the last part of #1612 - no longer need to special-case functional test scenarios  - the test infrastructure does that
    nit: remove unused `_serviceProvider` and constructor overload in `RazorPreCompiler`

--- src/Microsoft.AspNet.Mvc/RazorPreCompileModule.cs
+++ src/Microsoft.AspNet.Mvc/RazorPreCompileModule.cs
@@ -24,6 +24,7 @@ namespace Microsoft.AspNet.Mvc
         public RazorPreCompileModule(IServiceProvider services)
         {
             _appServices = services;
+
             // When ListenForMemoryPressure is true, the MemoryCache evicts items at every gen2 collection.
             // In DTH, gen2 happens frequently enough to make it undesirable for caching precompilation results. We'll
             // disable listening for memory pressure for the MemoryCache instance used by precompilation.
@@ -43,53 +44,23 @@ namespace Microsoft.AspNet.Mvc
             var compilerOptionsProvider = _appServices.GetRequiredService<ICompilerOptionsProvider>();
             var compilationSettings = compilerOptionsProvider.GetCompilationSettings(applicationEnvironment);
 
-            var setup = new RazorViewEngineOptionsSetup(applicationEnvironment);
-            var sc = new ServiceCollection();
-            sc.ConfigureOptions(setup);
-            sc.AddMvc();
+            // Create something similar to a HttpContext.RequestServices provider. Necessary because this class is
+            // instantiated in a lower-level "HttpContext.ApplicationServices" context. Most important added service
+            // is an IOptions<RazorViewEngineOptions> but use AddMvc() for simplicity and flexibility.
+            var serviceCollection = HostingServices.Create(_appServices);
+            serviceCollection.AddMvc();
+            var serviceProvider = serviceCollection.BuildServiceProvider();
 
-            var serviceProvider = BuildFallbackServiceProvider(sc, _appServices);
             var viewCompiler = new RazorPreCompiler(serviceProvider, context, _memoryCache, compilationSettings)
             {
                 GenerateSymbols = GenerateSymbols
             };
+
             viewCompiler.CompileViews();
         }
 
         public void AfterCompile(IAfterCompileContext context)
         {
-        }
-
-        // TODO: KILL THIS
-        private static IServiceProvider BuildFallbackServiceProvider(
-            IServiceCollection services,
-            IServiceProvider fallback)
-        {
-            var sc = HostingServices.Create(fallback);
-            sc.Add(services);
-
-            // Build the manifest
-            var manifestTypes = services.Where(t => t.ServiceType.GetTypeInfo().GenericTypeParameters.Length == 0
-                    && t.ServiceType != typeof(IServiceManifest)
-                    && t.ServiceType != typeof(IServiceProvider))
-                    .Select(t => t.ServiceType).Distinct();
-            sc.AddInstance<IServiceManifest>(
-                new ServiceManifest(manifestTypes, fallback.GetRequiredService<IServiceManifest>()));
-            return sc.BuildServiceProvider();
-        }
-
-        private class ServiceManifest : IServiceManifest
-        {
-            public ServiceManifest(IEnumerable<Type> services, IServiceManifest fallback = null)
-            {
-                Services = services;
-                if (fallback != null)
-                {
-                    Services = Services.Concat(fallback.Services).Distinct();
-                }
-            }
-
-            public IEnumerable<Type> Services { get; private set; }
         }
     }
 }
