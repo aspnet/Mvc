8302236211281e2ddae13d02291bbc35b5f4e9fc
Ryan Nowak <nowakra@gmail.com>
2015-07-30 11:14:28 -0700

    Fix #2874 part 1 - XML formatter support for IMvcBuilder

---
+++ src/Microsoft.AspNet.Mvc.Formatters.Xml/MvcXmlDataContractSerializerMvcOptionsSetup.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc.Formatters.Xml;
+using Microsoft.AspNet.Mvc.ModelBinding.Metadata;
+using Microsoft.Framework.OptionsModel;
+
+namespace Microsoft.AspNet.Mvc
+{
+    public class MvcXmlDataContractSerializerMvcOptionsSetup : ConfigureOptions<MvcOptions>
+    {
+        public MvcXmlDataContractSerializerMvcOptionsSetup()
+            : base(ConfigureMvc)
+        {
+            Order = DefaultOrder.DefaultFrameworkSortOrder + 10;
+        }
+
+        public static void ConfigureMvc(MvcOptions options)
+        {
+            options.ModelMetadataDetailsProviders.Add(new DataMemberRequiredBindingMetadataProvider());
+
+            options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+            options.InputFormatters.Add(new XmlDataContractSerializerInputFormatter());
+
+            options.ValidationExcludeFilters.Add(typeFullName: "System.Xml.Linq.XObject");
+            options.ValidationExcludeFilters.Add(typeFullName: "System.Xml.XmlNode");
+        }
+    }
+}
---
+++ src/Microsoft.AspNet.Mvc.Formatters.Xml/MvcXmlSerializerMvcOptionsSetup.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNet.Mvc.Formatters.Xml;
+using Microsoft.Framework.OptionsModel;
+
+namespace Microsoft.AspNet.Mvc
+{
+    public class MvcXmlSerializerMvcOptionsSetup : ConfigureOptions<MvcOptions>
+    {
+        public MvcXmlSerializerMvcOptionsSetup()
+            : base(ConfigureMvc)
+        {
+            Order = DefaultOrder.DefaultFrameworkSortOrder + 10;
+        }
+
+        public static void ConfigureMvc(MvcOptions options)
+        {
+            options.OutputFormatters.Add(new XmlSerializerOutputFormatter());
+            options.InputFormatters.Add(new XmlSerializerInputFormatter());
+        }
+    }
+}
