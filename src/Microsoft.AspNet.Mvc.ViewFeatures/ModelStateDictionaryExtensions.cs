c401c6ad9d3675935c77dffd7256d900d6a5ab62
Muchiachio <aldis.rameika@gmail.com>
2015-09-22 20:23:10 +0300

    Generic ModelStateDictionary add and remove extensions
    - Added generic add model error and remove extensions for
    ModelStateDictionary, to avoid using hard coded strings then specifying
    model state dictionary keys.
    
    aspnet/Mvc#3164

---
+++ src/Microsoft.AspNet.Mvc.ViewFeatures/ModelStateDictionaryExtensions.cs
@@ -0,0 +1,76 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Linq.Expressions;
+using Microsoft.AspNet.Mvc.ModelBinding;
+using Microsoft.AspNet.Mvc.ViewFeatures;
+
+namespace Microsoft.AspNet.Mvc
+{
+    /// <summary>
+    /// Extensions methods for <see cref="ModelStateDictionary"/>.
+    /// </summary>
+    public static class ModelStateDictionaryExtensions
+    {
+        /// <summary>
+        /// Adds the specified <paramref name="errorMessage"/> to the <see cref="ModelState.Errors"/> instance
+        /// that is associated with the specified <paramref name="expression"/> text key repressentation.
+        /// </summary>
+        /// <typeparam name="TModel">The type of the model.</typeparam>
+        /// <param name="modelState">The <see cref="ModelStateDictionary"/> instance this method extends.</param>
+        /// <param name="expression">The expression of <typeparamref name="TModel"/> from
+        /// which text representation will be used as a key of the <see cref="ModelState"/> to add errors to.</param>
+        /// <param name="errorMessage"></param>
+        public static void AddModelError<TModel>(this ModelStateDictionary modelState, Expression<Func<TModel, object>> expression, string errorMessage)
+        {
+            modelState.AddModelError(GetExpressionText(expression), errorMessage);
+        }
+
+        /// <summary>
+        /// Adds the specified <paramref name="exception"/> to the <see cref="ModelState.Errors"/> instance
+        /// that is associated with the specified <paramref name="expression"/> text key repressentation.
+        /// </summary>
+        /// <typeparam name="TModel">The type of the model.</typeparam>
+        /// <param name="modelState">The <see cref="ModelStateDictionary"/> instance this method extends.</param>
+        /// <param name="expression">The expression of <typeparamref name="TModel"/> from
+        /// which text representation will be used as a key of the <see cref="ModelState"/> to add errors to.</param>
+        /// <param name="exception">The error message to add.</param>
+        public static void AddModelError<TModel>(this ModelStateDictionary modelState, Expression<Func<TModel, object>> expression, Exception exception)
+        {
+            modelState.AddModelError(GetExpressionText(expression), exception);
+        }
+
+        /// <summary>
+        /// Removes the specified <paramref name="expression"/> text key repressentation from the <see cref="ModelStateDictionary"/>.
+        /// </summary>
+        /// <typeparam name="TModel">The type of the model.</typeparam>
+        /// <param name="modelState">The <see cref="ModelStateDictionary"/> instance this method extends.</param>
+        /// <param name="expression">The expression of <typeparamref name="TModel"/> from
+        /// which text representation will be used as a key of the <see cref="ModelState"/> to add errors to.</param>
+        /// <returns>
+        /// true if the element is successfully removed; otherwise, false.
+        /// This method also returns false if expression's text key was not found in the model-state dictionary.
+        /// </returns>
+        public static bool Remove<TModel>(this ModelStateDictionary modelState, Expression<Func<TModel, object>> expression)
+        {
+            return modelState.Remove(GetExpressionText(expression));
+        }
+
+        private static string GetExpressionText(LambdaExpression expression)
+        {
+            var unaryExpression = expression.Body as UnaryExpression;
+
+            if (IsConvertToObject(unaryExpression))
+                return ExpressionHelper.GetExpressionText(Expression.Lambda(unaryExpression.Operand, expression.Parameters[0]));
+
+            return ExpressionHelper.GetExpressionText(expression);
+        }
+        private static bool IsConvertToObject(UnaryExpression expression)
+        {
+            return expression?.NodeType == ExpressionType.Convert &&
+                expression.Operand is MemberExpression &&
+                expression.Type == typeof(object);
+        }
+    }
+}
---
+++ test/Microsoft.AspNet.Mvc.ViewFeatures.Test/ModelStateDictionaryExtensionsTest.cs
@@ -0,0 +1,241 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using Microsoft.AspNet.Mvc.ModelBinding;
+using Xunit;
+
+namespace Microsoft.AspNet.Mvc
+{
+    public class ModelStateDictionaryExtensionsTest
+    {
+        [Fact]
+        public void AddsModelErrorMessage_ForSingleExpression()
+        {
+            // Arrange
+            var modelState = new ModelStateDictionary();
+
+            // Act
+            modelState.AddModelError<TestModel>(model => model.Text, "Message");
+
+            // Assert
+            Assert.Equal("Text", modelState.Single().Key);
+            Assert.Equal("Message", modelState.Single().Value.Errors.Single().ErrorMessage);
+        }
+
+        [Fact]
+        public void AddsModelErrorMessage_ForRelationExpression()
+        {
+            // Arrange
+            var modelState = new ModelStateDictionary();
+
+            // Act
+            modelState.AddModelError<TestModel>(model => model.Child.Text, "Message");
+
+            // Assert
+            Assert.Equal("Child.Text", modelState.Single().Key);
+            Assert.Equal("Message", modelState.Single().Value.Errors.Single().ErrorMessage);
+        }
+
+        [Fact]
+        public void AddsModelErrorMessage_ForImplicitlyCastedToObjectExpression()
+        {
+            // Arrange
+            var modelState = new ModelStateDictionary();
+
+            // Act
+            modelState.AddModelError<TestModel>(model => model.Child.Value, "Message");
+
+            // Assert
+            Assert.Equal("Child.Value", modelState.Single().Key);
+            Assert.Equal("Message", modelState.Single().Value.Errors.Single().ErrorMessage);
+        }
+
+        [Fact]
+        public void AddsModelErrorMessage_ForExplicitlyCastedToObjectExpression()
+        {
+            // Arrange
+            var modelState = new ModelStateDictionary();
+
+            // Act
+            modelState.AddModelError<TestModel>(model => (object)model.Child.Value, "Message");
+
+            // Assert
+            Assert.Equal("Child.Value", modelState.Single().Key);
+            Assert.Equal("Message", modelState.Single().Value.Errors.Single().ErrorMessage);
+        }
+
+        [Fact]
+        public void AddsModelErrorMessage_ForExpressionWithoutTextRepresentation()
+        {
+            // Arrange
+            var modelState = new ModelStateDictionary();
+
+            // Act
+            modelState.AddModelError<TestModel>(model => model.ToString(), "Message");
+
+            // Assert
+            Assert.Equal("", modelState.Single().Key);
+            Assert.Equal("Message", modelState.Single().Value.Errors.Single().ErrorMessage);
+        }
+
+        [Fact]
+        public void AddsModelErrorException_ForSingleExpression()
+        {
+            // Arrange
+            var exception = new Exception();
+            var modelState = new ModelStateDictionary();
+
+            // Act
+            modelState.AddModelError<TestModel>(model => model.Text, exception);
+
+            // Assert
+            Assert.Equal("Text", modelState.Single().Key);
+            Assert.Same(exception, modelState.Single().Value.Errors.Single().Exception);
+        }
+
+        [Fact]
+        public void AddsModelErrorException_ForRelationExpression()
+        {
+            // Arrange
+            var exception = new Exception();
+            var modelState = new ModelStateDictionary();
+
+            // Act
+            modelState.AddModelError<TestModel>(model => model.Child.Text, exception);
+
+            // Assert
+            Assert.Equal("Child.Text", modelState.Single().Key);
+            Assert.Same(exception, modelState.Single().Value.Errors.Single().Exception);
+        }
+
+        [Fact]
+        public void AddsModelErrorException_ForImplicitlyCastedToObjectExpression()
+        {
+            // Arrange
+            var exception = new Exception();
+            var modelState = new ModelStateDictionary();
+
+            // Act
+            modelState.AddModelError<TestModel>(model => model.Child.Value, exception);
+
+            // Assert
+            Assert.Equal("Child.Value", modelState.Single().Key);
+            Assert.Same(exception, modelState.Single().Value.Errors.Single().Exception);
+        }
+
+        [Fact]
+        public void AddsModelErrorException_ForExplicitlyCastedToObjectExpression()
+        {
+            // Arrange
+            var exception = new Exception();
+            var modelState = new ModelStateDictionary();
+
+            // Act
+            modelState.AddModelError<TestModel>(model => (object)model.Child.Value, exception);
+
+            // Assert
+            Assert.Equal("Child.Value", modelState.Single().Key);
+            Assert.Same(exception, modelState.Single().Value.Errors.Single().Exception);
+        }
+
+        [Fact]
+        public void AddsModelErrorException_ForExpressionWithoutTextRepresentation()
+        {
+            // Arrange
+            var exception = new Exception();
+            var modelState = new ModelStateDictionary();
+
+            // Act
+            modelState.AddModelError<TestModel>(model => model.ToString(), exception);
+
+            // Assert
+            Assert.Equal("", modelState.Single().Key);
+            Assert.Same(exception, modelState.Single().Value.Errors.Single().Exception);
+        }
+
+        [Fact]
+        public void RemovesModelStateKey_ForSingleExpression()
+        {
+            // Arrange
+            var modelState = new ModelStateDictionary();
+            modelState.Add("Text", new ModelState());
+
+            // Act
+            modelState.Remove<TestModel>(model => model.Text);
+
+            // Assert
+            Assert.Empty(modelState);
+        }
+
+        [Fact]
+        public void RemovesModelStateKey_ForRelationExpression()
+        {
+            // Arrange
+            var modelState = new ModelStateDictionary();
+            modelState.Add("Child.Text", new ModelState());
+
+            // Act
+            modelState.Remove<TestModel>(model => model.Child.Text);
+
+            // Assert
+            Assert.Empty(modelState);
+        }
+
+        [Fact]
+        public void RemovesModelStateKey_ForImplicitlyCastedToObjectExpression()
+        {
+            // Arrange
+            var modelState = new ModelStateDictionary();
+            modelState.Add("Child.Value", new ModelState());
+
+            // Act
+            modelState.Remove<TestModel>(model => model.Child.Value);
+
+            // Assert
+            Assert.Empty(modelState);
+        }
+
+        [Fact]
+        public void RemovesModelStateKey_ForExplicitlyCastedToObjectExpression()
+        {
+            // Arrange
+            var modelState = new ModelStateDictionary();
+            modelState.Add("Child.Value", new ModelState());
+
+            // Act
+            modelState.Remove<TestModel>(model => (object)model.Child.Value);
+
+            // Assert
+            Assert.Empty(modelState);
+        }
+
+        [Fact]
+        public void RemovesModelStateKey_ForExpressionWithoutTextRepresentation()
+        {
+            // Arrange
+            var modelState = new ModelStateDictionary();
+            modelState.Add("", new ModelState());
+
+            // Act
+            modelState.Remove<TestModel>(model => model.ToString());
+
+            // Assert
+            Assert.Empty(modelState);
+        }
+
+        private class TestModel
+        {
+            public string Text { get; set; }
+
+            public ChildModel Child { get; set; }
+        }
+
+        private class ChildModel
+        {
+            public int Value { get; set; }
+            public string Text { get; set; }
+        }
+    }
+}
