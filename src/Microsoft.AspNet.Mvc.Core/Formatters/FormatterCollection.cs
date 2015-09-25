108a0098fc7a95a6380795e0d5c22f3d09564b9c
Søren Kruse <krusenen@gmail.com>
2015-09-25 21:23:33 +0200

    Add FormatterCollection<T>
    #2945

---
+++ src/Microsoft.AspNet.Mvc.Core/Formatters/FormatterCollection.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.AspNet.Mvc.Formatters
+{
+    public class FormatterCollection<TFormatter> : List<TFormatter>
+    {
+        /// <summary>
+        /// Removes all formatters of the specified type.
+        /// </summary>
+        /// <typeparam name="T">The type to remove.</typeparam>
+        public void RemoveType<T>() where T : TFormatter
+        {
+            RemoveAll(x => x is T);
+        }
+    }
+}
