1ba629592117e1d05eedc5ddd4e2d73401b543fb
Pranav K <prkrishn@hotmail.com>
2015-11-20 15:11:49 -0800

    Removing PageInstrumentation
    Fixes #3497

---
+++ test/WebSites/RazorPageExecutionInstrumentationWebSite/RazorPageDiagnosticListener.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.Extensions.DiagnosticAdapter;
+
+namespace RazorPageExecutionInstrumentationWebSite
+{
+    public class RazorPageDiagnosticListener
+    {
+        public List<string> PageInstrumentationData { get; } = new List<string>();
+
+        [DiagnosticName("Microsoft.AspNet.Mvc.Razor.BeginInstrumentationContext")]
+        public virtual void OnBeginPageInstrumentationContext(
+            string path,
+            int position,
+            int length,
+            bool isLiteral)
+        {
+            var literal = isLiteral ? "Literal" : "Non-literal";
+            PageInstrumentationData.Add($"{path}: {literal} at {position} contains {length} characters.");
+        }
+    }
+}
