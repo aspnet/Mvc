9bbbd08c71893641645840233b210932c9379bd5
Doug Bunting <dougbu@microsoft.com>
2015-11-23 16:44:33 -0800

    Update `IJsonHelper` to use `IHtmlContent` - part of #3123 (2 of 5 or so) - do not expose specific `HtmlString` class

--- src/Microsoft.AspNet.Mvc.ViewFeatures/Rendering/IJsonHelper.cs
+++ src/Microsoft.AspNet.Mvc.ViewFeatures/Rendering/IJsonHelper.cs
@@ -1,6 +1,7 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using Microsoft.AspNet.Html.Abstractions;
 using Newtonsoft.Json;
 
 namespace Microsoft.AspNet.Mvc.Rendering
@@ -15,7 +16,7 @@ namespace Microsoft.AspNet.Mvc.Rendering
         /// </summary>
         /// <param name="value">The value to serialize as JSON.</param>
         /// <returns>A new <see cref="HtmlString"/> containing the serialized JSON.</returns>
-        HtmlString Serialize(object value);
+        IHtmlContent Serialize(object value);
 
         /// <summary>
         /// Returns serialized JSON for the <paramref name="value"/>.
@@ -23,6 +24,6 @@ namespace Microsoft.AspNet.Mvc.Rendering
         /// <param name="value">The value to serialize as JSON.</param>
         /// <param name="serializerSettings">The <see cref="JsonSerializerSettings"/> to be used by the serializer.</param>
         /// <returns>A new <see cref="HtmlString"/> containing the serialized JSON.</returns>
-        HtmlString Serialize(object value, JsonSerializerSettings serializerSettings);
+        IHtmlContent Serialize(object value, JsonSerializerSettings serializerSettings);
     }
 }
