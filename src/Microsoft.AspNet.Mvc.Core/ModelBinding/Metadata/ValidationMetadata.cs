aea19418d38af50d90ca4aa8d583afb8c03ab6b7
Ryan Nowak <nowakra@gmail.com>
2015-12-08 18:22:10 -0800

    Remove IExcludeTypeFilter
    - Removes IExcludeTypeFilter
    - Replaced with a property 'ValidateChildren' on ModelMetadata
    - Teach ValidationVisitor to respect 'ValidateChildren' for enumerable
      types.

--- src/Microsoft.AspNet.Mvc.Core/ModelBinding/Metadata/ValidationMetadata.cs
+++ src/Microsoft.AspNet.Mvc.Core/ModelBinding/Metadata/ValidationMetadata.cs
@@ -19,6 +19,14 @@ namespace Microsoft.AspNet.Mvc.ModelBinding.Metadata
         public bool? IsRequired { get; set; }
 
         /// <summary>
+        /// Gets or sets a value that indicates whether children of the model should be validated. If <c>null</c>
+        /// then <see cref="ModelMetadata.ValidateChildren"/> will be computed based on the value of
+        /// <see cref="ModelMetadata.IsComplexType"/> and <see cref="ModelMetadata.IsEnumerableType"/>. By default
+        /// complex types and enumerable types support validation of children.
+        /// </summary>
+        public bool? ValidateChildren { get; set; }
+
+        /// <summary>
         /// Gets a list of metadata items for validators.
         /// </summary>
         /// <remarks>
---
+++ src/Microsoft.AspNet.Mvc.Core/ModelBinding/Validation/ValidationExcludeFilter.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Diagnostics;
+using System.Reflection;
+using Microsoft.AspNet.Mvc.ModelBinding.Metadata;
+
+namespace Microsoft.AspNet.Mvc.ModelBinding.Validation
+{
+    public class ValidationExcludeFilter : IValidationMetadataProvider
+    {
+        public ValidationExcludeFilter(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            Type = type;
+        }
+
+        public ValidationExcludeFilter(string typeFullName)
+        {
+            if (typeFullName == null)
+            {
+                throw new ArgumentNullException(nameof(typeFullName));
+            }
+
+            TypeFullName = typeFullName;
+        }
+
+        public Type Type { get; }
+
+        public string TypeFullName { get; }
+
+        public void GetValidationMetadata(ValidationMetadataProviderContext context)
+        {
+            if (Type != null)
+            {
+                if (Type.GetTypeInfo().IsAssignableFrom(context.Key.ModelType.GetTypeInfo()))
+                {
+                    context.ValidationMetadata.ValidateChildren = false;
+                }
+
+                return;
+            }
+
+            if (TypeFullName != null)
+            {
+                if (IsMatchingName(context.Key.ModelType))
+                {
+                    context.ValidationMetadata.ValidateChildren = false;
+                }
+
+                return;
+            }
+        }
+
+        private bool IsMatchingName(Type type)
+        {
+            Debug.Assert(TypeFullName != null);
+
+            if (type == null)
+            {
+                return false;
+            }
+
+            if (string.Equals(type.FullName, TypeFullName, StringComparison.Ordinal))
+            {
+                return true;
+            }
+
+            return IsMatchingName(type.GetTypeInfo().BaseType);
+        }
+    }
+}
