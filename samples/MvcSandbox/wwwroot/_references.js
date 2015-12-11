b956bf75454483b58839a7e3e777429d869bbcab
Kiran Challa <kichalla@microsoft.com>
2015-12-10 17:16:09 -0800

    Fix localization tests

--- samples/MvcSandbox/wwwroot/_references.js
+++ samples/MvcSandbox/wwwroot/_references.js
@@ -1,3 +1,5 @@
 ﻿/// <autosync enabled="true" />
 /// <reference path="../gulpfile.js" />
 /// <reference path="js/site.js" />
+/// <reference path="lib/jquery/dist/jquery.js" />
+/// <reference path="lib/jquery-validation-unobtrusive/jquery.validate.unobtrusive.js" />
