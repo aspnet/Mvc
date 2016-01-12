db942da95ce5bbbcece20e3ce1f26137d0999c88
Ryan Nowak <nowakra@gmail.com>
2016-01-12 12:39:38 -0800

    WQ

--- src/Microsoft.AspNet.Mvc.ViewFeatures/Buffer/ViewBuffer.cs
+++ src/Microsoft.AspNet.Mvc.ViewFeatures/Buffer/ViewBuffer.cs
@@ -2,9 +2,11 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
+using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Html;
@@ -208,6 +210,7 @@ namespace Microsoft.AspNet.Mvc.ViewFeatures.Buffer
                     if (valueAsHtmlContent != null)
                     {
                         valueAsHtmlContent.WriteTo(writer, encoder);
+                        await writer.FlushAsync();
                         continue;
                     }
                 }
@@ -215,5 +218,135 @@ namespace Microsoft.AspNet.Mvc.ViewFeatures.Buffer
         }
 
         private string DebuggerToString() => _name;
+
+        private class HtmlContentTextWriter : TextWriter
+        {
+            private const int PageSize = 1024;
+
+            private readonly TextWriter _inner;
+            private readonly List<char[]> _pages;
+            private readonly ArrayPool<char> _pool;
+
+            private int _currentPage;
+            private int _currentIndex; // The next 'free' character
+
+            public HtmlContentTextWriter(ArrayPool<char> pool, TextWriter inner)
+            {
+                _pool = pool;
+                _inner = inner;
+                _pages = new List<char[]>();
+            }
+
+            public override Encoding Encoding => _inner.Encoding;
+
+            public override void Flush()
+            {
+                // Don't do anything. We'll call FlushAsync.
+            }
+
+            public override async Task FlushAsync()
+            {
+                for (var i = 0; i <= _currentPage; i++)
+                {
+                    var page = _pages[i];
+
+                    var count = i == _currentPage ? _currentIndex : page.Length;
+                    await _inner.WriteAsync(page, 0, count);
+                }
+
+                _currentPage = 0;
+                _currentIndex = 0;
+            }
+
+            public override void Write(char value)
+            {
+                var page = GetCurrentPage();
+                page[_currentIndex++] = value;
+            }
+
+            public override void Write(char[] buffer)
+            {
+                Write(buffer, 0, buffer.Length);
+            }
+
+            public override void Write(char[] buffer, int index, int count)
+            {
+                while (count > 0)
+                {
+                    var page = GetCurrentPage();
+                    var copyLength = page.Length - _currentIndex;
+                    Debug.Assert(copyLength > 0);
+
+                    System.Buffer.BlockCopy(
+                        buffer,
+                        index, 
+                        page,
+                        _currentIndex,
+                        copyLength);
+
+                    _currentIndex += copyLength;
+                    index += copyLength;
+
+                    count -= copyLength;
+                }
+            }
+
+            public override void Write(string value)
+            {
+                var index = 0;
+                var count = value.Length;
+
+                while (count > 0)
+                {
+                    var page = GetCurrentPage();
+                    var copyLength = page.Length - _currentIndex;
+                    Debug.Assert(copyLength > 0);
+
+                    value.CopyTo(
+                        index,
+                        page,
+                        _currentIndex,
+                        copyLength);
+
+                    _currentIndex += copyLength;
+                    index += copyLength;
+
+                    count -= copyLength;
+                }
+            }
+
+            private char[] GetCurrentPage()
+            {
+                var page = _pages.Count == 0 ? null : _pages[_currentPage];
+                if (page == null || _currentIndex == page.Length)
+                {
+                    // Current page is full.
+                    _currentPage++;
+                    _currentIndex = 0;
+
+                    if (_pages.Count > _currentPage)
+                    {
+                        page = _pages[_currentPage];
+                    }
+                    else
+                    {
+                        page = _pool.Rent(PageSize);
+                        _pages.Add(page);
+                    }
+                }
+
+                return page;
+            }
+
+            protected override void Dispose(bool disposing)
+            {
+                base.Dispose(disposing);
+
+                for (var i = 0; i < _pages.Count; i++)
+                {
+                    _pool.Return(_pages[i]);
+                }
+            }
+        }
     }
 }
