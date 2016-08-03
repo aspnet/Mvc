d5333ac30cfffc21178234d8a903f68dc36dc6ae
Pranav K <prkrishn@hotmail.com>
2016-08-03 16:53:07 -0700

    Razor debug information uses relative path
    Fixes https://github.com/aspnet/Razor/issues/809

---
+++ src/Microsoft.AspNetCore.Mvc.Razor.Host/IMvcRazorHostWithCodeGenerationContext.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Razor;
+using Microsoft.AspNetCore.Razor.CodeGenerators;
+
+namespace Microsoft.AspNetCore.Mvc.Razor
+{
+    /// <summary>
+    /// Specifies the contracts for a Razor host that parses Razor files and generates C# code.
+    /// </summary>
+    public interface IMvcRazorHostWithTemplateEngineContext
+    {
+        /// <summary>
+        /// Parses and generates the contents of a Razor file.
+        /// </summary>
+        /// <returns>The <see cref="GeneratorResults"/>.</returns>
+        GeneratorResults GenerateCode(TemplateEngineContext context);
+    }
+}
