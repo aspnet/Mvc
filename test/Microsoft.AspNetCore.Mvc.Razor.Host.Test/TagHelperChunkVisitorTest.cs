e4de9fc5a500500eea7eee849ec56617b1352076
Crystal Qian <crystaljqian@gmail.com>
2016-08-29 18:23:43 -0700

    Finished tests.

--- test/Microsoft.AspNetCore.Mvc.Razor.Host.Test/TagHelperChunkVisitorTest.cs
+++ test/Microsoft.AspNetCore.Mvc.Razor.Host.Test/TagHelperChunkVisitorTest.cs
@@ -1,11 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
 using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Razor.Host.Test.Internal;
+using Microsoft.AspNetCore.Razor.Chunks;
+using Microsoft.AspNetCore.Razor.Parser.SyntaxTree;
+using Moq;
+using Xunit;
 
 namespace Microsoft.AspNetCore.Mvc.Razor.Host.Test
 {
     public class TagHelperChunkVisitorTest
     {
+        [Fact]
+        public void Accept_CorrectlyDecoratesViewComponentChunks()
+        {
+            // Arrange
+            var codeGeneratorContext = ChunkVisitorTestFactory.CreateDummyCodeGeneratorContext();
+            var syntaxTreeNode = new Mock<Span>(new SpanBuilder());
+            foreach (var chunk in ChunkVisitorTestFactory.GetTestChunks(visitedTagHelperChunks: false))
+            {
+                codeGeneratorContext.ChunkTreeBuilder.AddChunk(chunk, syntaxTreeNode.Object);
+            }
+
+            var tagHelperChunkVisitor = new TagHelperChunkVisitor(codeGeneratorContext);
+            var expectedChunks = ChunkVisitorTestFactory.GetTestChunks(visitedTagHelperChunks: true);
+
+            // Act
+            var resultChunks = codeGeneratorContext.ChunkTreeBuilder.Root.Children;
+            tagHelperChunkVisitor.Accept(resultChunks);
+
+            // Assert
+            // Test the normal tag helper chunk, Baz.
+            Assert.Equal(expectedChunks.Count(), resultChunks.Count());
+
+            var expectedTagHelperChunk = expectedChunks.ElementAt(0) as TagHelperChunk;
+            var resultTagHelperChunk = resultChunks.ElementAt(0) as TagHelperChunk;
+            Assert.NotNull(resultTagHelperChunk);
+
+            Assert.Equal(expectedTagHelperChunk.Descriptors.First().TypeName,
+                         resultTagHelperChunk.Descriptors.First().TypeName,
+                         StringComparer.Ordinal);
+
+            // Test the parent chunk with view component tag helper inside, Foo.
+            var expectedParentChunk = expectedChunks.ElementAt(1) as ParentChunk;
+            var resultParentChunk = expectedChunks.ElementAt(1) as ParentChunk;
+            Assert.NotNull(resultParentChunk);
+            Assert.Single(resultParentChunk.Children);
+
+            expectedTagHelperChunk = expectedParentChunk.Children.First() as TagHelperChunk;
+            resultTagHelperChunk = resultParentChunk.Children.First() as TagHelperChunk;
+
+            Assert.Equal(expectedTagHelperChunk.Descriptors.First().TypeName,
+                resultTagHelperChunk.Descriptors.First().TypeName,
+                StringComparer.Ordinal);
+
+            // Test the view component tag helper, Bar.
+            expectedTagHelperChunk = expectedChunks.ElementAt(2) as TagHelperChunk;
+            resultTagHelperChunk = resultChunks.ElementAt(2) as TagHelperChunk;
+            Assert.NotNull(resultTagHelperChunk);
+
+            Assert.Equal(expectedTagHelperChunk.Descriptors.First().TypeName,
+                 resultTagHelperChunk.Descriptors.First().TypeName,
+                 StringComparer.Ordinal);
+        }
     }
 }
