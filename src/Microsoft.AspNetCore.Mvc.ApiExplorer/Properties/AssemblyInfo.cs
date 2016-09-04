6145d18ba5fad9ab1312c8889d0451c321d83ef0
Oswald Maskens <ozie@live.be>
2016-09-04 14:36:34 -0400

    Changed AssemlbyCopyright to reflect the copyright on top of the file. Fixes #5231

--- src/Microsoft.AspNetCore.Mvc.ApiExplorer/Properties/AssemblyInfo.cs
+++ src/Microsoft.AspNetCore.Mvc.ApiExplorer/Properties/AssemblyInfo.cs
@@ -6,6 +6,6 @@ using System.Resources;
 
 [assembly: AssemblyMetadata("Serviceable", "True")]
 [assembly: NeutralResourcesLanguage("en-us")]
-[assembly: AssemblyCompany("Microsoft Corporation.")]
-[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
+[assembly: AssemblyCompany(".NET Foundation.")]
+[assembly: AssemblyCopyright("© .NET Foundation. All rights reserved. Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.")]
 [assembly: AssemblyProduct("Microsoft ASP.NET Core")]
