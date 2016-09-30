019fa32fab24b4cc60da32c320c2112b14df8587
Chris Rosa <Chris Rosa@DESKTOP-7T9R4G2>
2016-09-30 11:52:51 -0400

    Fixed using order

--- test/WebSites/TagHelpersWebSite/TagHelpers/DictionaryPrefixTestTagHelper.cs
+++ test/WebSites/TagHelpersWebSite/TagHelpers/DictionaryPrefixTestTagHelper.cs
@@ -1,10 +1,10 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
-using System.Collections.Generic;
 
 namespace TagHelpersWebSite.TagHelpers
 {
