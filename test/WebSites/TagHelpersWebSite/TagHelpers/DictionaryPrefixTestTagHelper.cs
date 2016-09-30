0ee96fbce92178f29476e66b01f3f19e26b1721e
Chris Rosa <Chris Rosa@DESKTOP-7T9R4G2>
2016-09-30 11:49:05 -0400

    PR tweaks

--- test/WebSites/TagHelpersWebSite/TagHelpers/DictionaryPrefixTestTagHelper.cs
+++ test/WebSites/TagHelpersWebSite/TagHelpers/DictionaryPrefixTestTagHelper.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Microsoft.AspNetCore.Razor.TagHelpers;
 using System.Collections.Generic;
 
-namespace Microsoft.AspNetCore.Mvc.Razor.Host.Test
+namespace TagHelpersWebSite.TagHelpers
 {
     [HtmlTargetElement(Attributes = "prefix-*")]
     public class DictionaryPrefixTestTagHelper : TagHelper
