b8588cde48e3f5a1aa669610bae12006017d5b0c
Jass Bagga <jbagga93@live.ca>
2016-09-30 17:42:44 -0700

    Added 3 AcceptedResult classes. Working on Tests for AcceptedObjectResult and AcceptedAtActionResult

---
+++ test/Microsoft.AspNetCore.Mvc.Core.Test/AcceptedObjectResultTests.cs
@@ -0,0 +1,108 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Buffers;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.Internal;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging.Testing;
+using Moq;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.Core.Test
+{
+    public class AcceptedObjectResultTests
+    {
+
+        [Fact]
+        public void AcceptedObjectResult_SetsLocation()
+        {
+            // Arrange
+            var location = "http://test/location";
+
+            // Act
+            var result = new AcceptedObjectResult(location, "testInput");
+
+            // Assert
+            Assert.Same(location, result.Location);
+        }
+
+        [Fact]
+        public async Task AcceptedObjectResult_ReturnsStatusCode_SetsLocationHeader()
+        {
+            // Arrange
+            var location = "/test/";
+            var httpContext = GetHttpContext();
+            var actionContext = GetActionContext(httpContext);
+            var result = new AcceptedObjectResult(location, "testInput");
+
+            // Act
+            await result.ExecuteResultAsync(actionContext);
+
+            // Assert
+            Assert.Equal(StatusCodes.Status202Accepted, httpContext.Response.StatusCode);
+            Assert.Equal(location, httpContext.Response.Headers["Location"]);
+        }
+
+        [Fact]
+        public async Task AcceptedObjectResult_OverwritesLocationHeader()
+        {
+            // Arrange
+            var location = "/test/";
+            var httpContext = GetHttpContext();
+            var actionContext = GetActionContext(httpContext);
+            httpContext.Response.Headers["Location"] = "/different/location/";
+            var result = new AcceptedObjectResult(location, "testInput");
+
+            // Act
+            await result.ExecuteResultAsync(actionContext);
+
+            // Assert
+            Assert.Equal(StatusCodes.Status202Accepted, httpContext.Response.StatusCode);
+            Assert.Equal(location, httpContext.Response.Headers["Location"]);
+        }
+
+        private static ActionContext GetActionContext(HttpContext httpContext)
+        {
+            var routeData = new RouteData();
+            routeData.Routers.Add(Mock.Of<IRouter>());
+
+            return new ActionContext(httpContext,
+                                    routeData,
+                                    new ActionDescriptor());
+        }
+
+        private static HttpContext GetHttpContext()
+        {
+            var httpContext = new DefaultHttpContext();
+            httpContext.Request.PathBase = new PathString("");
+            httpContext.Response.Body = new MemoryStream();
+            httpContext.RequestServices = CreateServices();
+            return httpContext;
+        }
+
+        private static IServiceProvider CreateServices()
+        {
+            var options = new TestOptionsManager<MvcOptions>();
+            options.Value.OutputFormatters.Add(new StringOutputFormatter());
+            options.Value.OutputFormatters.Add(new JsonOutputFormatter(
+                new JsonSerializerSettings(),
+                ArrayPool<char>.Shared));
+
+            var services = new ServiceCollection();
+            services.AddSingleton(new ObjectResultExecutor(
+                options,
+                new TestHttpResponseStreamWriterFactory(),
+                NullLoggerFactory.Instance));
+
+            return services.BuildServiceProvider();
+        }
+    }
+}
