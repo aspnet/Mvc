61042488f3db4e8d9dcba28058b2507213a8e58f
Alexander Paskhin <paskhin@hotmail.co.uk>
2016-10-22 21:28:41 +0100

    Add UtitTest case for XmlResult. Code optimization.

--- src/Microsoft.AspNetCore.Mvc.Formatters.Xml/Internal/XmlDcResultExecutor.cs
+++ src/Microsoft.AspNetCore.Mvc.Formatters.Xml/Internal/XmlDcResultExecutor.cs
@@ -16,17 +16,12 @@ using Microsoft.AspNetCore.Http;
 namespace Microsoft.AspNetCore.Mvc.Formatters.Xml.Internal
 {
     /// <summary>
-    /// Executes a <see cref="XmlResult"/> to write to the response.
+    /// Executes a <see cref="XmlResult"/> to write to the DataContract xml response.
     /// </summary>
     public class XmlDcResultExecutor : IXmlResultExecutor
     {
-        private static readonly string DefaultContentType = new MediaTypeHeaderValue("application/xml")
-        {
-            Encoding = Encoding.UTF8
-        }.ToString();
-
         /// <summary>
-        /// Creates a new <see cref="XmlDcResultExecutor"/>.
+        /// Creates a new <see cref="XmlResultExecutor"/>.
         /// </summary>
         /// <param name="writerFactory">The <see cref="IHttpResponseStreamWriterFactory"/>.</param>
         /// <param name="loggerFactory">The <see cref="ILoggerFactory"/>.</param>
@@ -44,78 +39,32 @@ namespace Microsoft.AspNetCore.Mvc.Formatters.Xml.Internal
                 throw new ArgumentNullException(nameof(loggerFactory));
             }
 
-
-
             WriterFactory = writerFactory;
-            Logger = loggerFactory.CreateLogger<XmlResult>();
-
+            LoggerFactory = loggerFactory;
         }
 
         /// <summary>
+        /// Gets the <see cref="XmlResultExecutorBase"/>.
+        /// </summary>
+        XmlResultExecutorBase XmlResultExecutorBase { get; }
+        /// <summary>
         /// Gets the <see cref="ILogger"/>.
         /// </summary>
-        protected ILogger Logger { get; }
-
+        protected ILoggerFactory LoggerFactory { get; }
 
         /// <summary>
         /// Gets the <see cref="IHttpResponseStreamWriterFactory"/>.
         /// </summary>
         protected IHttpResponseStreamWriterFactory WriterFactory { get; }
 
-        /// <summary>
-        /// Executes the <see cref="XmlResult"/> and writes the response.
-        /// </summary>
-        /// <param name="context">The <see cref="ActionContext"/>.</param>
-        /// <param name="result">The <see cref="XmlResult"/>.</param>
-        /// <returns>A <see cref="Task"/> which will complete when writing has completed.</returns>
         public Task ExecuteAsync(ActionContext context, XmlResult result)
         {
-            if (context == null)
-            {
-                throw new ArgumentNullException(nameof(context));
-            }
-
-            if (result == null)
-            {
-                throw new ArgumentNullException(nameof(result));
-            }
-
-            var response = context.HttpContext.Response;
-
-            string resolvedContentType = null;
-            Encoding resolvedContentTypeEncoding = null;
-            ResponseContentTypeHelper.ResolveContentTypeAndEncoding(
-                                                    result.ContentType,
-                                                    response.ContentType,
-                                                    DefaultContentType,
-                                                    out resolvedContentType,
-                                                    out resolvedContentTypeEncoding);
-
-            response.ContentType = resolvedContentType;
-
-            if (result.StatusCode != null)
-            {
-                response.StatusCode = result.StatusCode.Value;
-            }
-
             var serializerSettings = result.XmlSerializerSettings ?? FormattingUtilities.GetDefaultXmlWriterSettings();
-
             TextOutputFormatter formatter;
             // create the proper formatter
             formatter = new XmlDataContractSerializerOutputFormatter(serializerSettings);
-            var outputFormatterWriterContext = new OutputFormatterWriteContext(
-                                                        context.HttpContext,
-                                                        WriterFactory.CreateWriter,
-                                                        result.Value.GetType(), result.Value);
-
-            outputFormatterWriterContext.ContentType = new StringSegment(resolvedContentType);
-
-            //  Logger formatter and value of object
-
-            Logger.FormatterSelected(formatter, outputFormatterWriterContext);
-            Logger.XmlResultExecuting(result.Value);
-
-            return formatter.WriteAsync(outputFormatterWriterContext);
+            XmlResultExecutorBase xmlBase = new XmlResultExecutorBase(WriterFactory, LoggerFactory, formatter);
+            return xmlBase.ExecuteAsync(context, result);
         }
     }
 }
