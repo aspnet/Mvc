a7b4a8bfaf1e7657fd7e20efcaaf3a4fad99648c
Marcus Schweda <marcus.schweda@googlemail.com>
2016-11-03 22:06:35 +0100

    Comment

--- src/Microsoft.AspNetCore.Mvc.Core/ApplicationParts/AssemblyPart.cs
+++ src/Microsoft.AspNetCore.Mvc.Core/ApplicationParts/AssemblyPart.cs
@@ -49,20 +49,22 @@ namespace Microsoft.AspNetCore.Mvc.ApplicationParts
         {
             if (!Assembly.IsDynamic)
             {
-                var dependencyContext = DependencyContext.Load(Assembly);
-                if (dependencyContext != null)
-                {
-                    return dependencyContext.CompileLibraries.SelectMany(library => library.ResolveReferencePaths());
-                }
-
-                // If an application has been compiled without preserveCompilationContext, return the path to the assembly
-                // as a reference. For runtime compilation, this will allow the compilation to succeed as long as it least
-                // one application part has been compiled with preserveCompilationContext and contains a super set of types
-                // required for the compilation to succeed.
-                return new[] { Assembly.Location };
+                // Skip loading process for dynamic assemblies. This prevents DependencyContextLoader from reading the
+                // .deps.json file from either manifest resources or the assembly location, which will fail.
+                return Enumerable.Empty<string>();
             }
 
-            return Enumerable.Empty<string>();
+            var dependencyContext = DependencyContext.Load(Assembly);
+            if (dependencyContext != null)
+            {
+                return dependencyContext.CompileLibraries.SelectMany(library => library.ResolveReferencePaths());
+            }
+
+            // If an application has been compiled without preserveCompilationContext, return the path to the assembly
+            // as a reference. For runtime compilation, this will allow the compilation to succeed as long as it least
+            // one application part has been compiled with preserveCompilationContext and contains a super set of types
+            // required for the compilation to succeed.
+            return new[] { Assembly.Location };
         }
     }
 }
