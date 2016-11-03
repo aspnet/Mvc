850bd6993650b1d7503ab5868057d697b8d829e3
TB <t0mburton@users.noreply.github.com>
2016-11-03 11:59:11 +0000

    Modify missing view component message

--- src/Microsoft.AspNetCore.Mvc.ViewFeatures/Properties/Resources.Designer.cs
+++ src/Microsoft.AspNetCore.Mvc.ViewFeatures/Properties/Resources.Designer.cs
@@ -75,7 +75,8 @@ namespace Microsoft.AspNetCore.Mvc.ViewFeatures
         }
 
         /// <summary>
-        /// A view component named '{0}' could not be found.
+        /// A view component named '{0}' could not be found. View components must be a Public non-generic concrete type, 
+        /// not contains generic parameters, use ViewComponentAttribute or class name end with 'ViewComponent' 
         /// </summary>
         internal static string ViewComponent_CannotFindComponent
         {
@@ -83,7 +84,8 @@ namespace Microsoft.AspNetCore.Mvc.ViewFeatures
         }
 
         /// <summary>
-        /// A view component named '{0}' could not be found.
+        /// A view component named '{0}' could not be found. View components must be a Public non-generic concrete type, 
+        /// not contains generic parameters, use ViewComponentAttribute or class name end with 'ViewComponent' 
         /// </summary>
         internal static string FormatViewComponent_CannotFindComponent(object p0)
         {
