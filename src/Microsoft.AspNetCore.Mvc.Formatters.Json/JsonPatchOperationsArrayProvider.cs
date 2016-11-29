8ab358fbc3a2f4a7fb380512e3e2ae6329a20b6d
Jass Bagga <jbagga93@live.ca>
2016-11-29 14:38:27 -0800

    [Design] Mutate parameter type for API descriptions from JsonPatchDocument<T> to Operation[]

---
+++ src/Microsoft.AspNetCore.Mvc.Formatters.Json/JsonPatchOperationsArrayProvider.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Reflection;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.JsonPatch.Operations;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace Microsoft.AspNetCore.Mvc.Formatters.Json
+{
+    public class JsonPatchOperationsArrayProvider : IApiDescriptionProvider
+    {
+        /// <inheritdoc />
+        public int Order
+        {
+            get { return -999; }
+        }
+
+        /// <inheritdoc />
+        public void OnProvidersExecuting(ApiDescriptionProviderContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+
+            foreach (var result in context.Results)
+            {
+                foreach (var parameterDescription in result.ParameterDescriptions)
+                {
+                    if (typeof(IJsonPatchDocument).GetTypeInfo().IsAssignableFrom(parameterDescription.Type))
+                    {
+                        parameterDescription.Type = typeof(Operation[]);
+                    }
+                }
+            }
+        }
+
+        public void OnProvidersExecuted(ApiDescriptionProviderContext context)
+        {
+        }
+    }
+}
---
+++ test/Microsoft.AspNetCore.Mvc.Formatters.Json.Test/JsonPatchOperationsArrayProviderTests.cs
@@ -0,0 +1,49 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.JsonPatch.Operations;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Mvc.Formatters.Json.Test
+{
+    public class JsonPatchOperationsArrayProviderTests
+    {
+        [Fact]
+        public void OnProvidersExecuting_FindsJsonPatchDocuments_ProvidesOperationsArray()
+        {
+            // Arrange
+            var provider = new JsonPatchOperationsArrayProvider();
+            var jsonPatchParameterDescription = new ApiParameterDescription
+            {
+                Type = typeof(JsonPatchDocument)
+            };
+
+            var stringParameterDescription = new ApiParameterDescription
+            {
+                Type = typeof(string)
+            };
+
+            var apiDescription = new ApiDescription();
+            apiDescription.ParameterDescriptions.Clear();
+            apiDescription.ParameterDescriptions.Add(jsonPatchParameterDescription);
+            apiDescription.ParameterDescriptions.Add(stringParameterDescription);
+
+            var actionDescriptorList = new List<ActionDescriptor>();
+            var apiDescriptionProviderContext = new ApiDescriptionProviderContext(actionDescriptorList);
+            apiDescriptionProviderContext.Results.Add(apiDescription);
+
+            // Act
+            provider.OnProvidersExecuting(apiDescriptionProviderContext);
+
+            // Assert
+            Assert.Equal(2, apiDescription.ParameterDescriptions.Count);
+            Assert.True(apiDescription.ParameterDescriptions.Any(d => d.Type == typeof(Operation[])));
+            Assert.False(apiDescription.ParameterDescriptions.Any(d => d.Type == typeof(IJsonPatchDocument)));
+        }
+    }
+}
