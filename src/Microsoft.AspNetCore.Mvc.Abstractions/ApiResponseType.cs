c643bd8f0be6c1d944b6400e5fbbfe7d7a050689
Jass Bagga <jbagga93@live.ca>
2016-12-19 14:40:02 -0800

    Undo comment deletion

--- src/Microsoft.AspNetCore.Mvc.Abstractions/ApiResponseType.cs
+++ src/Microsoft.AspNetCore.Mvc.Abstractions/ApiResponseType.cs
@@ -29,7 +29,9 @@ namespace Microsoft.AspNetCore.Mvc.ApiExplorer
         /// Gets or sets the CLR data type of the response or null.
         /// </summary>
         /// <remarks>
-        /// Will be null if the action returns no response, or if the response type is unclear.
+        /// Will be null if the action returns no response, or if the response type is unclear. Use
+        /// <c>Microsoft.AspNetCore.Mvc.ProducesAttribute</c> or <c>Microsoft.AspNetCore.Mvc.ProducesResponseTypeAttribute</c> on an action method
+        /// to specify a reponse type.
         /// </remarks>
         public Type Type { get; set; }
 
