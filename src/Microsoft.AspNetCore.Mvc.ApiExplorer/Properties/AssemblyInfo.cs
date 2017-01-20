7987a3c9fd9ef259ba401ce909b87ba9f4634839
N. Taylor Mullen <nimullen@microsoft.com>
2017-01-20 12:13:27 -0800

    Convert MVC to CSproj.
    - Allow >= RC3 CLI's to build and run MVC.
    - Added extra sln's so they could be opened in VS. Currently VS' project system can't currently handle Mvc.sln.
    - Worked around several dotnet migration issues. They are listed in the re-attempted migration section here: https://github.com/aspnet/Mvc/issues/5482
    - One large feature bit that couldn't be worked around was the functional tests running on desktop; it represented several known vstest issues. Removed desktop running of functional tests.
    - Skipped an ActionContextAccessor test due to a vstest appdomain problem.

--- src/Microsoft.AspNetCore.Mvc.ApiExplorer/Properties/AssemblyInfo.cs
+++ src/Microsoft.AspNetCore.Mvc.ApiExplorer/Properties/AssemblyInfo.cs
@@ -2,14 +2,9 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Reflection;
-using System.Resources;
 using System.Runtime.CompilerServices;
 
 [assembly: AssemblyMetadata("Serviceable", "True")]
-[assembly: NeutralResourcesLanguage("en-us")]
-[assembly: AssemblyCompany("Microsoft Corporation.")]
-[assembly: AssemblyCopyright("© Microsoft Corporation. All rights reserved.")]
-[assembly: AssemblyProduct("Microsoft ASP.NET Core")]
 [assembly: TypeForwardedTo(typeof(Microsoft.AspNetCore.Mvc.ApiExplorer.IApiDescriptionProvider))]
 [assembly: TypeForwardedTo(typeof(Microsoft.AspNetCore.Mvc.ApiExplorer.ApiDescription))]
 [assembly: TypeForwardedTo(typeof(Microsoft.AspNetCore.Mvc.ApiExplorer.ApiDescriptionProviderContext))]
