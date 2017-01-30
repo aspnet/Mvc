bc1cb67d1b61518dfd418e52106e15bd5708aa76
Ryan Brandenburg <rybrande@microsoft.com>
2017-01-30 10:53:53 -0800

    PR cleanup

--- src/Microsoft.AspNetCore.Mvc/HostingApplicationNameProvider.cs
+++ src/Microsoft.AspNetCore.Mvc/HostingApplicationNameProvider.cs
@@ -1,4 +1,7 @@
-﻿using System.Linq;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Linq;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Localization;
--- src/Microsoft.AspNetCore.Mvc/MvcServiceCollectionExtensions.cs
+++ src/Microsoft.AspNetCore.Mvc/MvcServiceCollectionExtensions.cs
@@ -4,7 +4,6 @@
 using System;
 using System.Linq;
 using System.Reflection;
-using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.AspNetCore.Mvc.Internal;
