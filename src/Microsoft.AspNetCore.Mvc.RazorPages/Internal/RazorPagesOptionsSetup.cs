c05e2b19f15cadb8128e4a92445a7f9c9f32d868
Pranav K <prkrishn@hotmail.com>
2017-02-27 15:49:12 -0800

    Add convenience extension methods for IPageApplicationModelConvention

---
+++ src/Microsoft.AspNetCore.Mvc.RazorPages/Internal/RazorPagesOptionsSetup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc.ViewFeatures.Internal;
+
+namespace Microsoft.AspNetCore.Mvc.RazorPages.Internal
+{
+    public class RazorPagesOptionsSetup : IConfigureOptions<RazorPagesOptions>
+    {
+        public void Configure(RazorPagesOptions options)
+        {
+            if (options == null)
+            {
+                throw new ArgumentNullException(nameof(options));
+            }
+
+            // Support for [TempData] on properties
+            options.ConfigureFilter(new SaveTempDataPropertyFilter());
+            // Always require an antiforgery token on post
+            options.ConfigureFilter(new AutoValidateAntiforgeryTokenAttribute());
+        }
+    }
+}
