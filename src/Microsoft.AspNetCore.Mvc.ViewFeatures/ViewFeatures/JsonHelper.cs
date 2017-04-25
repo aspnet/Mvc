2c18c4fbdc5636bc717a382d8affefb25983ef1e
N. Taylor Mullen <nimullen@microsoft.com>
2017-04-25 12:23:36 -0700

    Update JsonHelper to escape HTML.
    - This functionality can be disabled by setting the `Switch.Microsoft.AspNetCore.Mvc.AllowJsonHtml` switch in an app.config.
    - Updated tests to react to this new behavior.
    - Exposed a new `PublicSerializerSettings` property on `JsonOutputFormatter` so we can accurately copy settings from the used output formatter in `JsonHelper`.

--- src/Microsoft.AspNetCore.Mvc.ViewFeatures/ViewFeatures/JsonHelper.cs
+++ src/Microsoft.AspNetCore.Mvc.ViewFeatures/ViewFeatures/JsonHelper.cs
@@ -3,8 +3,14 @@
 
 using System;
 using System.Buffers;
+#if NET451
+using System.Configuration;
+#endif
 using System.Globalization;
 using System.IO;
+#if NET451
+using System.Linq;
+#endif
 using Microsoft.AspNetCore.Html;
 using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -17,6 +23,7 @@ namespace Microsoft.AspNetCore.Mvc.ViewFeatures
     /// </summary>
     public class JsonHelper : IJsonHelper
     {
+        private const string AllowJsonHtml = "Switch.Microsoft.AspNetCore.Mvc.AllowJsonHtml";
         private readonly JsonOutputFormatter _jsonOutputFormatter;
         private readonly ArrayPool<char> _charPool;
 
@@ -46,7 +53,23 @@ namespace Microsoft.AspNetCore.Mvc.ViewFeatures
         /// <inheritdoc />
         public IHtmlContent Serialize(object value)
         {
-            return SerializeInternal(_jsonOutputFormatter, value);
+            var allowJsonHtml = false;
+#if NET451
+            var switchValue = ConfigurationManager.AppSettings.GetValues(AllowJsonHtml)?.FirstOrDefault();
+            bool.TryParse(switchValue, out allowJsonHtml);
+#else
+            AppContext.TryGetSwitch(AllowJsonHtml, out allowJsonHtml);
+#endif
+
+            if (allowJsonHtml)
+            {
+                return SerializeInternal(_jsonOutputFormatter, value);
+            }
+
+            var settings = ShallowCopy(_jsonOutputFormatter.PublicSerializerSettings);
+            settings.StringEscapeHandling = StringEscapeHandling.EscapeHtml;
+
+            return Serialize(value, settings);
         }
 
         /// <inheritdoc />
@@ -68,6 +91,44 @@ namespace Microsoft.AspNetCore.Mvc.ViewFeatures
             jsonOutputFormatter.WriteObject(stringWriter, value);
 
             return new HtmlString(stringWriter.ToString());
+        }
+
+        private static JsonSerializerSettings ShallowCopy(JsonSerializerSettings settings)
+        {
+            var copiedSettings = new JsonSerializerSettings
+            {
+                Binder = settings.Binder,
+                CheckAdditionalContent = settings.CheckAdditionalContent,
+                ConstructorHandling = settings.ConstructorHandling,
+                Context = settings.Context,
+                ContractResolver = settings.ContractResolver,
+                Converters = settings.Converters,
+                Culture = settings.Culture,
+                DateFormatHandling = settings.DateFormatHandling,
+                DateFormatString = settings.DateFormatString,
+                DateParseHandling = settings.DateParseHandling,
+                DateTimeZoneHandling = settings.DateTimeZoneHandling,
+                DefaultValueHandling = settings.DefaultValueHandling,
+                EqualityComparer = settings.EqualityComparer,
+                Error = settings.Error,
+                FloatFormatHandling = settings.FloatFormatHandling,
+                FloatParseHandling = settings.FloatParseHandling,
+                Formatting = settings.Formatting,
+                MaxDepth = settings.MaxDepth,
+                MetadataPropertyHandling = settings.MetadataPropertyHandling,
+                MissingMemberHandling = settings.MissingMemberHandling,
+                NullValueHandling = settings.NullValueHandling,
+                ObjectCreationHandling = settings.ObjectCreationHandling,
+                PreserveReferencesHandling = settings.PreserveReferencesHandling,
+                ReferenceLoopHandling = settings.ReferenceLoopHandling,
+                ReferenceResolverProvider = settings.ReferenceResolverProvider,
+                StringEscapeHandling = settings.StringEscapeHandling,
+                TraceWriter = settings.TraceWriter,
+                TypeNameAssemblyFormat = settings.TypeNameAssemblyFormat,
+                TypeNameHandling = settings.TypeNameHandling,
+            };
+
+            return copiedSettings;
         }
     }
 }
