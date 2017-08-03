de80691f7dbceadce19c3e20a60c036808859db2
Pranav K <prkrishn@hotmail.com>
2017-08-03 16:41:57 -0700

    First draft of tool

---
+++ src/Mvc.KodKod.Tool/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.Extensions.CommandLineUtils;
+
+namespace Mvc.KodKod.Tool
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var app = new CommandLineApplication();
+            var appName = app.Option("--application-name", "Application name", CommandOptionType.SingleValue);
+            var outputPath = app.Option("--output-path", "Output path", CommandOptionType.SingleValue);
+            var applicationBaseUrl = app.Option("--base-url", "The url application is hosted at.", CommandOptionType.SingleValue);
+
+            app.OnExecute(() =>
+            {
+                try
+                {
+                    var kodKod = new KodKod
+                    {
+                        ApplicationName = appName.Value(),
+                        OutputPath = outputPath.Value(),
+                        BaseUrl = applicationBaseUrl.Value(),
+                    };
+                    kodKod.Execute();
+                }
+                catch (Exception ex)
+                {
+                    Console.Error.WriteLine($"[error]: {ex}");
+                    return 1;
+                }
+
+                return 0;
+            });
+
+            app.Execute(args);
+        }
+    }
+}
---
+++ src/Mvc.KodKod.Tool/SwaggerOperation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace Mvc.KodKod.Tool
+{
+    public class SwaggerOperation
+    {
+        public string Method { get; set; }
+
+        public string Type { get; set; }
+
+        public List<SwaggerParameter> Parameters { get; } = new List<SwaggerParameter>();
+    }
+}
