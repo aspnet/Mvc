916b9c1e469782aada72dec9bbb200b086273997
Ryan Nowak <nowakra@gmail.com>
2017-12-22 10:57:55 -0800

    improve docs, add example

--- src/Microsoft.AspNetCore.Mvc.Core/CompatibilityVersion.cs
+++ src/Microsoft.AspNetCore.Mvc.Core/CompatibilityVersion.cs
@@ -1,29 +1,52 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Microsoft.AspNetCore.Mvc
 {
     /// <summary>
-    /// Specifies the version compatibility of settings configured by <see cref="MvcOptions"/>. 
+    /// Specifies the version compatibility of runtime behaviors configured by <see cref="MvcOptions"/>. 
     /// </summary>
     /// <remarks>
-    /// Setting <see cref="MvcCompatibilityOptions.CompatibilityVersion"/> to a specific version will 
-    /// change the default values of various settings to match a particular release of ASP.NET Core MVC. 
-    /// See <see cref="MvcCompatibilityOptions.CompatibilityVersion"/> for more details.
+    /// <para>
+    /// The best way to set a compatibility version is by using 
+    /// <see cref="MvcCoreMvcBuilderExtensions.SetCompatibilityVersion"/> or
+    /// <see cref="MvcCoreMvcCoreBuilderExtensions.SetCompatibilityVersion"/> in your application's
+    /// <c>ConfigureServices</c> method.
+    /// <example>
+    /// Setting the compatibility version using <see cref="IMvcBuilder"/>:
+    /// <code>
+    /// public class Startup
+    /// {
+    ///     ...
+    ///     
+    ///     public void ConfigureServices(IServiceCollection services)
+    ///     {
+    ///         services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_0);
+    ///     }
+    ///     
+    ///     ...
+    /// }
+    /// </code>
+    /// </example>
+    /// </para>
+    /// <para>
+    /// Setting compatiblity version to a specific version will change the default values of various
+    /// settings to match a particular minor release of ASP.NET Core MVC. 
+    /// </para>
     /// </remarks>
     public enum CompatibilityVersion
     {
         /// <summary>
         /// Sets the default value of settings on <see cref="MvcOptions"/> to match the behavior of 
-        /// ASP.NET Core MVC 2.0.0.
+        /// ASP.NET Core MVC 2.0.
         /// </summary>
         Version_2_0,
 
         /// <summary>
         /// Sets the default value of settings on <see cref="MvcOptions"/> to match the behavior of 
-        /// ASP.NET Core MVC 2.1.0.
+        /// ASP.NET Core MVC 2.1.
         /// </summary>
         Version_2_1,
 
