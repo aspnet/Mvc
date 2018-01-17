6bcc0c44640fba70f6c0520214cfb31da1b7f5b9
Javier Calvarro Nelson <jacalvar@microsoft.com>
2018-01-17 14:46:45 -0800

    Further samples

--- samples/MvcSandbox/Startup.cs
+++ samples/MvcSandbox/Startup.cs
@@ -1,9 +1,13 @@
 // Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Diagnostics;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.ApplicationParts;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
@@ -14,7 +18,15 @@ namespace MvcSandbox
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+                .ConfigureApplicationPartManager(apm => {
+                    Debug.Assert(!apm.ApplicationParts.Any(ap => ap.Name == "MvcSandbox.PrecompiledViews.dll"));
+                    var precompiledViewsPath = Path.Combine(
+                        Path.GetDirectoryName(typeof(Startup).Assembly.Location),
+                        "MvcSandbox.PrecompiledViews.dll");
+
+                    apm.ApplicationParts.Add(new AssemblyPart(Assembly.LoadFile(precompiledViewsPath)));
+                });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
