d143b7a48bc069c9691491209aded10ca3c145b8
treenew <treenew@sofire.org>
2018-01-29 10:20:13 +0800

    Add page action executor provider.

---
+++ src/Microsoft.AspNetCore.Mvc.RazorPages/Internal/DefaultPageActionExectuorProvider.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+
+namespace Microsoft.AspNetCore.Mvc.RazorPages.Internal
+{
+    class DefaultPageActionExectuorProvider : IPageActionExectuorProvider
+    {
+        public Func<object, object[], Task<IActionResult>>[] GetExecutors(CompiledPageActionDescriptor actionDescriptor)
+        {
+
+            if(actionDescriptor.HandlerMethods == null || actionDescriptor.HandlerMethods.Count == 0)
+            {
+                return Array.Empty<Func<object, object[], Task<IActionResult>>>();
+            }
+
+            var results = new Func<object, object[], Task<IActionResult>>[actionDescriptor.HandlerMethods.Count];
+
+            for(var i = 0; i < actionDescriptor.HandlerMethods.Count; i++)
+            {
+                results[i] = ExecutorFactory.CreateExecutor(actionDescriptor.HandlerMethods[i]);
+            }
+
+            return results;
+        }
+    }
+}
--- src/Microsoft.AspNetCore.Mvc.RazorPages/Internal/PageActionInvokerProvider.cs
+++ src/Microsoft.AspNetCore.Mvc.RazorPages/Internal/PageActionInvokerProvider.cs
@@ -28,6 +28,7 @@ namespace Microsoft.AspNetCore.Mvc.RazorPages.Internal
 
         private readonly IPageLoader _loader;
         private readonly IPageFactoryProvider _pageFactoryProvider;
+        private readonly IPageActionExectuorProvider _pageActionExecutorProvider;
         private readonly IPageModelFactoryProvider _modelFactoryProvider;
         private readonly IRazorPageFactoryProvider _razorPageFactoryProvider;
         private readonly IActionDescriptorCollectionProvider _collectionProvider;
@@ -187,7 +188,7 @@ namespace Microsoft.AspNetCore.Mvc.RazorPages.Internal
 
             var viewStartFactories = GetViewStartFactories(compiledActionDescriptor);
 
-            var executors = GetExecutors(compiledActionDescriptor);
+            var executors = _pageActionExecutorProvider.GetExecutors(compiledActionDescriptor);
 
             return new PageActionInvokerCacheEntry(
                 compiledActionDescriptor,
@@ -220,23 +221,6 @@ namespace Microsoft.AspNetCore.Mvc.RazorPages.Internal
             }
 
             return viewStartFactories;
-        }
-
-        private static Func<object, object[], Task<IActionResult>>[] GetExecutors(CompiledPageActionDescriptor actionDescriptor)
-        {
-            if (actionDescriptor.HandlerMethods == null || actionDescriptor.HandlerMethods.Count == 0)
-            {
-                return Array.Empty<Func<object, object[], Task<IActionResult>>>();
-            }
-
-            var results = new Func<object, object[], Task<IActionResult>>[actionDescriptor.HandlerMethods.Count];
-
-            for (var i = 0; i < actionDescriptor.HandlerMethods.Count; i++)
-            {
-                results[i] = ExecutorFactory.CreateExecutor(actionDescriptor.HandlerMethods[i]);
-            }
-
-            return results;
         }
 
         internal class InnerCache
