06d63a7d956ff4722033179bafac381eafb0289c
Pranav K <prkrishn@hotmail.com>
2018-05-01 15:05:51 -0700

    Stuff

--- build/buildpipeline/pipeline.groovy
+++ build/buildpipeline/pipeline.groovy
@@ -1,15 +1,15 @@
 import org.dotnet.ci.pipelines.Pipeline
+load "build/buildpipeline/configuration.groovy"
 
 def windowsPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/windows.groovy')
 def linuxPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/linux.groovy')
 def osxPipeline = Pipeline.createPipeline(this, 'build/buildpipeline/osx.groovy')
+def aspnetConfig = new AspNetConfiguration()
 String configuration = 'Release'
-def parameters = [
-    'Configuration': configuration
-]
+def parameters = [ 'Configuration': configuration]
 
-windowsPipeline.triggerPipelineOnEveryGithubPR("Windows ${configuration} x64 Build", parameters)
-windowsPipeline.triggerPipelineOnGithubPush(parameters)
+windowsPipeline.triggerPipelineOnEveryGithubPR("Windows ${configuration} x64 Build", [ 'config': aspnetConfig, configuration: 'Release'])
+windowsPipeline.triggerPipelineOnGithubPush([ 'config': aspnetConfig, configuration: 'Release'])
 
 linuxPipeline.triggerPipelineOnEveryGithubPR("Ubuntu 16.04 ${configuration} Build", parameters)
 linuxPipeline.triggerPipelineOnGithubPush(parameters)
