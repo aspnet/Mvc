e06e419183e398a82e23d5e10efa71255556ef97
Pranav K <prkrishn@hotmail.com>
2018-05-03 15:44:53 -0700

    Cleanup CachedExpressionCompiler

--- src/Microsoft.AspNetCore.Mvc.ViewFeatures/Internal/CachedExpressionCompiler.cs
+++ src/Microsoft.AspNetCore.Mvc.ViewFeatures/Internal/CachedExpressionCompiler.cs
@@ -6,9 +6,9 @@ using System.Collections.Concurrent;
 using System.Linq.Expressions;
 using System.Reflection;
 
-namespace Microsoft.AspNetCore.Mvc.ViewFeatures.Internal
+namespace Microsoft.AspNetCore.Mvc.ViewFeatures
 {
-    public static class CachedExpressionCompiler
+    internal static class CachedExpressionCompiler
     {
         // This is the entry point to the cached expression compilation system. The system
         // will try to turn the expression into an actual delegate as quickly as possible,
@@ -38,117 +38,109 @@ namespace Microsoft.AspNetCore.Mvc.ViewFeatures.Internal
 
             public static Func<TModel, TResult> Compile(Expression<Func<TModel, TResult>> expression)
             {
-                if (expression == null)
+                switch (expression.Body)
                 {
-                    throw new ArgumentNullException(nameof(expression));
-                }
+                    // model => model
+                    case var body when body == expression.Parameters[0]:
+                        return CompileFromIdentityFunc(expression);
 
-                return CompileFromIdentityFunc(expression)
-                       ?? CompileFromConstLookup(expression)
-                       ?? CompileFromMemberAccess(expression)
-                       ?? CompileSlow(expression);
+                    // model => (object){const}
+                    case ConstantExpression constantExpression:
+                        return CompileFromConstLookup(constantExpression);
+
+                    // model => CapturedConstant
+                    case MemberExpression memberExpression when memberExpression.Expression is ConstantExpression constantExpression:
+                        return CompileCapturedConstant(memberExpression, constantExpression);
+
+                    // model => StaticMember
+                    case MemberExpression memberExpression when memberExpression.Expression == null:
+                        return CompileFromStaticMemberAccess(expression, memberExpression);
+
+                    // model => model.Member
+                    case MemberExpression memberExpression when memberExpression.Expression == expression.Parameters[0]:
+                        return CompileFromMemberAccess(expression, memberExpression);
+
+                    default:
+                        return CompileSlow(expression);
+                }
             }
 
             private static Func<TModel, TResult> CompileFromConstLookup(
-                Expression<Func<TModel, TResult>> expression)
+                ConstantExpression constantExpression)
             {
-                if (expression == null)
-                {
-                    throw new ArgumentNullException(nameof(expression));
-                }
-
-                var constantExpression = expression.Body as ConstantExpression;
-                if (constantExpression != null)
-                {
-                    // model => {const}
-
-                    var constantValue = (TResult)constantExpression.Value;
-                    return _ => constantValue;
-                }
-
-                return null;
+                // model => {const}
+                var constantValue = (TResult)constantExpression.Value;
+                return _ => constantValue;
             }
 
             private static Func<TModel, TResult> CompileFromIdentityFunc(
                 Expression<Func<TModel, TResult>> expression)
             {
-                if (expression == null)
+                // model => model
+
+                // Don't need to lock, as all identity funcs are identical.
+                if (_identityFunc == null)
                 {
-                    throw new ArgumentNullException(nameof(expression));
+                    _identityFunc = expression.Compile();
                 }
 
-                if (expression.Body == expression.Parameters[0])
-                {
-                    // model => model
-
-                    // Don't need to lock, as all identity funcs are identical.
-                    if (_identityFunc == null)
-                    {
-                        _identityFunc = expression.Compile();
-                    }
-
-                    return _identityFunc;
-                }
-
-                return null;
+                return _identityFunc;
             }
 
             private static Func<TModel, TResult> CompileFromMemberAccess(
-                Expression<Func<TModel, TResult>> expression)
+                Expression<Func<TModel, TResult>> expression,
+                MemberExpression memberExpression)
             {
-                if (expression == null)
+                // model => model.Member
+                if (_simpleMemberAccessCache.TryGetValue(memberExpression.Member, out var result))
                 {
-                    throw new ArgumentNullException(nameof(expression));
+                    return result;
                 }
 
-                // Performance tests show that on the x64 platform, special-casing static member and
-                // captured local variable accesses is faster than letting the fingerprinting system
-                // handle them. On the x86 platform, the fingerprinting system is faster, but only
-                // by around one microsecond, so it's not worth it to complicate the logic here with
-                // an architecture check.
+                result = expression.Compile();
+                result = _simpleMemberAccessCache.GetOrAdd(memberExpression.Member, result);
+                return result;
+            }
 
-                var memberExpression = expression.Body as MemberExpression;
-                if (memberExpression != null)
+            private static Func<TModel, TResult> CompileFromStaticMemberAccess(
+                Expression<Func<TModel, TResult>> expression,
+                MemberExpression memberExpression)
+            {
+                // model => model.StaticMember
+                if (_simpleMemberAccessCache.TryGetValue(memberExpression.Member, out var result))
                 {
-                    if (memberExpression.Expression == expression.Parameters[0] || memberExpression.Expression == null)
-                    {
-                        // model => model.Member or model => StaticMember
-                        return _simpleMemberAccessCache.GetOrAdd(memberExpression.Member, _ => expression.Compile());
-                    }
-
-                    var constantExpression = memberExpression.Expression as ConstantExpression;
-                    if (constantExpression != null)
-                    {
-                        // model => {const}.Member (captured local variable)
-                        var compiledExpression = _constMemberAccessCache.GetOrAdd(memberExpression.Member, _ =>
-                        {
-                            // rewrite as capturedLocal => ((TDeclaringType)capturedLocal).Member
-                            var parameterExpression = Expression.Parameter(typeof(object), "capturedLocal");
-                            var castExpression =
-                                Expression.Convert(parameterExpression, memberExpression.Member.DeclaringType);
-                            var replacementMemberExpression = memberExpression.Update(castExpression);
-                            var replacementExpression = Expression.Lambda<Func<object, TResult>>(
-                                replacementMemberExpression,
-                                parameterExpression);
-
-                            return replacementExpression.Compile();
-                        });
-
-                        var capturedLocal = constantExpression.Value;
-                        return _ => compiledExpression(capturedLocal);
-                    }
+                    return result;
                 }
 
-                return null;
+                result = expression.Compile();
+                result = _simpleMemberAccessCache.GetOrAdd(memberExpression.Member, result);
+                return result;
+            }
+
+            private static Func<TModel, TResult> CompileCapturedConstant(MemberExpression memberExpression, ConstantExpression constantExpression)
+            {
+                // model => {const}.Member (captured local variable)
+                if (!_constMemberAccessCache.TryGetValue(memberExpression.Member, out var result))
+                {
+                    // rewrite as capturedLocal => ((TDeclaringType)capturedLocal).Member
+                    var parameterExpression = Expression.Parameter(typeof(object), "capturedLocal");
+                    var castExpression =
+                        Expression.Convert(parameterExpression, memberExpression.Member.DeclaringType);
+                    var replacementMemberExpression = memberExpression.Update(castExpression);
+                    var replacementExpression = Expression.Lambda<Func<object, TResult>>(
+                        replacementMemberExpression,
+                        parameterExpression);
+
+                    result = replacementExpression.Compile();
+                    result = _constMemberAccessCache.GetOrAdd(memberExpression.Member, result);
+                }
+
+                var capturedLocal = constantExpression.Value;
+                return _ => result(capturedLocal);
             }
 
             private static Func<TModel, TResult> CompileSlow(Expression<Func<TModel, TResult>> expression)
             {
-                if (expression == null)
-                {
-                    throw new ArgumentNullException(nameof(expression));
-                }
-
                 // fallback compilation system - just compile the expression directly
                 return expression.Compile();
             }
