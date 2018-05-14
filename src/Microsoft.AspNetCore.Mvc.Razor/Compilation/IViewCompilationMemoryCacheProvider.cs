70cd2616ac38786d6276ccce1ce8703654566667
Sebastien Ros <sebastienros@gmail.com>
2018-05-14 16:48:07 -0700

    Feedback

--- src/Microsoft.AspNetCore.Mvc.Razor/Compilation/IViewCompilationMemoryCacheProvider.cs
+++ src/Microsoft.AspNetCore.Mvc.Razor/Compilation/IViewCompilationMemoryCacheProvider.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.Extensions.Caching.Memory;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Microsoft.AspNetCore.Mvc.Razor.Compilation
 {
     /// <summary>
-    /// Provides an instance of <see cref="IMemoryCache"/> that is used to stop compiled Razor views. 
+    /// Provides an instance of <see cref="IMemoryCache"/> that is used to store compiled Razor views. 
     /// </summary>
     public interface IViewCompilationMemoryCacheProvider
     {
--- src/Microsoft.AspNetCore.Mvc.Razor/Compilation/RazorViewCompilationMemoryCacheProvider.cs
+++ src/Microsoft.AspNetCore.Mvc.Razor/Compilation/RazorViewCompilationMemoryCacheProvider.cs
@@ -1,11 +1,11 @@
-﻿using Microsoft.Extensions.Caching.Memory;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.Extensions.Caching.Memory;
 
 namespace Microsoft.AspNetCore.Mvc.Razor.Compilation
 {
-    /// <summary>
-    /// And implementation of <see cref="IViewCompilationMemoryCacheProvider"/> using a local <see cref="IMemoryCache"/> instance.
-    /// </summary>
-    public class RazorViewCompilationMemoryCacheProvider : IViewCompilationMemoryCacheProvider
+    internal class RazorViewCompilationMemoryCacheProvider : IViewCompilationMemoryCacheProvider
     {
         IMemoryCache IViewCompilationMemoryCacheProvider.CompilationMemoryCache { get; } = new MemoryCache(new MemoryCacheOptions());
     }
