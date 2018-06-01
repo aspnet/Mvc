fabac0e4c33dd86841f8b229d27e816c962b10c8
Pranav K <prkrishn@hotmail.com>
2018-06-01 14:29:34 -0700

    Create an analyzer to warn users not to decorate filters on page handler methods
    Fixes #7684

---
+++ test/Microsoft.AspNetCore.Mvc.Analyzers.Test/TestFiles/AttributesShouldNotBeAppliedToPageModelAnalyzerTest/NoDiagnosticsAreReturned_ForNonHandlerMethodsWithAttributes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Microsoft.AspNetCore.Mvc.Analyzers.Test
+{
+    public class NoDiagnosticsAreReturned_ForNonHandlerMethodsWithAttributes : PageModel
+    {
+        public void OnGet() => OnGet();
+
+        [Authorize]
+        private void OnGetPrivate() { }
+
+        [TypeFilter(typeof(object))]
+        internal IActionResult OnPost() => null;
+
+        [AllowAnonymous]
+        public void OnGet<T>() { }
+
+        [ServiceFilter(typeof(object))]
+        public static void OnPostStatic() { }
+    }
+}
