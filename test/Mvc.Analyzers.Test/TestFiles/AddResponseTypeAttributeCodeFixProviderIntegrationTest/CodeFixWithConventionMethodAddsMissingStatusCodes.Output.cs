17803ed10427a53face31b54f90d1a14b54f0e38
Pranav K <prkrishn@hotmail.com>
2018-07-10 10:17:42 -0700

    Add a code fix that applies ProducesResponseTypeAttributes

---
+++ test/Mvc.Analyzers.Test/TestFiles/AddResponseTypeAttributeCodeFixProviderIntegrationTest/CodeFixWithConventionMethodAddsMissingStatusCodes.Output.cs
@@ -0,0 +1,21 @@
+﻿namespace Microsoft.AspNetCore.Mvc.Analyzers._OUTPUT_
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class CodeFixWithConventionMethodAddsMissingStatusCodes : ControllerBase
+    {
+        [ApiConventionMethod(typeof(DefaultApiConventions), nameof(DefaultApiConventions.Find))]
+        [ProducesResponseType(202)]
+        [ProducesResponseType(404)]
+        [ProducesDefaultResponseType]
+        public ActionResult<string> GetItem(int id)
+        {
+            if (id == 0)
+            {
+                return NotFound();
+            }
+
+            return Accepted("Result");
+        }
+    }
+}
