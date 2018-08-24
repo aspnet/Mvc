5ca03fdf9069b975cf17c435900d83aa25320427
Alexej Timonin <alexejtimonin@gmail.com>
2018-08-24 15:57:36 +0200

    Add PageRemoteAttribute
    Addresses #8245

---
+++ src/Microsoft.AspNetCore.Mvc.ViewFeatures/PageRemoteAttribute.cs
@@ -0,0 +1,90 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.Extensions.DependencyInjection;
+using Resources = Microsoft.AspNetCore.Mvc.ViewFeatures.Resources;
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    /// <summary>
+    /// A <see cref="RemoteAttribute"/> for razor page handler which configures Unobtrusive validation 
+    /// to send an Ajax request to the web site. The invoked handler should return JSON indicating 
+    /// whether the value is valid.
+    /// </summary>
+    /// <remarks>Does no server-side validation of the final form submission.</remarks>
+    public class PageRemoteAttribute : RemoteAttribute
+    {
+        private readonly string _pageHandler;
+        private readonly string _pageName;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PageRemoteAttribute"/> class.
+        /// </summary>
+        /// <remarks>
+        /// Will use ambient page name and handler name when generating the URL where client
+        /// should send a validation request.
+        /// </remarks>
+        public PageRemoteAttribute()
+            : this(pageHandler: null)
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PageRemoteAttribute"/> class.
+        /// </summary>
+        /// <param name="pageHandler">
+        /// The handler name used when generating the URL where client should send a validation request.
+        /// </param>
+        public PageRemoteAttribute(string pageHandler)
+            : this(pageHandler, pageName: null)
+        { }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PageRemoteAttribute"/> class.
+        /// </summary>
+        /// <param name="pageHandler">
+        /// The page handler name used when generating the URL where client should send a validation request.
+        /// </param>
+        /// <param name="pageName">
+        /// The page name used when generating the URL where client should send a validation request.
+        /// </param>
+        /// <remarks>
+        /// <para>
+        /// If <paramref name="pageName"/> or <paramref name="pageHandler"/> is <c>null</c>, uses the corresponding
+        /// ambient value.
+        /// </para>
+        /// </remarks>
+        public PageRemoteAttribute(string pageHandler, string pageName)
+            : base()
+        {
+            _pageHandler = pageHandler;
+            _pageName = pageName;
+        }
+
+        /// <inheritdoc />
+        protected override string GetUrl(ClientModelValidationContext context)
+        {
+            if (context == null)
+            {
+                throw new ArgumentNullException(nameof(context));
+            }
+            
+            var services = context.ActionContext.HttpContext.RequestServices;
+            var factory = services.GetRequiredService<IUrlHelperFactory>();
+            var urlHelper = factory.GetUrlHelper(context.ActionContext);
+
+            var url = urlHelper.Page(_pageName, _pageHandler);
+
+            if (url == null)
+            {
+                throw new InvalidOperationException(Resources.RemoteAttribute_NoUrlFound);
+            }
+
+            return url;
+        }
+    }
+}
---
+++ test/Microsoft.AspNetCore.Mvc.ViewFeatures.Test/PageRemoteAttributeTest.cs
@@ -0,0 +1,124 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Routing;
+using Microsoft.AspNetCore.Testing;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+using Xunit;
+using Resources = Microsoft.AspNetCore.Mvc.ViewFeatures.Resources;
+
+namespace Microsoft.AspNetCore.Mvc
+{
+    public class PageRemoteAttributeTest
+    {
+        private static readonly IModelMetadataProvider _metadataProvider = new EmptyModelMetadataProvider();
+        private static readonly ModelMetadata _metadata = _metadataProvider.GetMetadataForProperty(
+            typeof(string),
+            nameof(string.Length));
+
+        [Fact]
+        public void GetUrl_WhenUrlHelperReturnsNull_Throws()
+        {
+            // Arrange
+            var testablePageRemoteAttribute = new TestablePageRemoteAttribute("pageHandler", "pageName");
+            var actionContext = GetActionContextForPage("/pageName");
+            
+            var mockUrlHelper = CreateMockUrlHelper(actionContext, returnUrl: null);
+            var validationContext = new ClientModelValidationContext(
+                actionContext, 
+                _metadata, 
+                _metadataProvider, 
+                new AttributeDictionary());
+
+            var serviceCollection = new ServiceCollection();
+            var urlHelperFactory = new Mock<IUrlHelperFactory>();
+            urlHelperFactory.Setup(f => f.GetUrlHelper(actionContext))
+                .Returns(mockUrlHelper.Object);
+            serviceCollection.AddSingleton<IUrlHelperFactory>(urlHelperFactory.Object);
+            var serviceProvider = serviceCollection.BuildServiceProvider();
+
+            actionContext.HttpContext = new DefaultHttpContext
+            {
+                RequestServices = serviceProvider
+            };
+            
+            // Act && Assert
+            ExceptionAssert.Throws<InvalidOperationException>(
+                () => testablePageRemoteAttribute.InvokeGetUrl(validationContext),
+                Resources.RemoteAttribute_NoUrlFound);
+        }
+
+
+        private static ActionContext GetActionContextForPage(string page)
+        {
+            return new ActionContext
+            {
+                ActionDescriptor = new ActionDescriptor
+                {
+                    RouteValues = new Dictionary<string, string>
+                    {
+                        { "page", page },
+                    },
+                },
+                RouteData = new RouteData
+                {
+                    Routers = { Mock.Of<IRouter>() },
+                    Values = { ["page"] = page },
+                },
+            };
+        }
+
+        private static Mock<IUrlHelper> CreateMockUrlHelper(ActionContext context, string returnUrl)
+        {
+            var urlHelper = new Mock<IUrlHelper>();
+            urlHelper.SetupGet(h => h.ActionContext)
+                .Returns(context);
+            urlHelper.Setup(h => h.RouteUrl(It.IsAny<UrlRouteContext>()))
+                .Returns(returnUrl);
+            return urlHelper;
+        }
+        
+        private class TestablePageRemoteAttribute : PageRemoteAttribute
+        {
+            public TestablePageRemoteAttribute(string pageHandler)
+                : base(pageHandler)
+            {
+            }
+
+            public TestablePageRemoteAttribute(string pageHandler, string pageName)
+                : base(pageHandler, pageName)
+            {
+            }
+            
+            public new string RouteName
+            {
+                get
+                {
+                    return base.RouteName;
+                }
+            }
+
+            public new RouteValueDictionary RouteData
+            {
+                get
+                {
+                    return base.RouteData;
+                }
+            }
+
+            public string InvokeGetUrl(ClientModelValidationContext context)
+            {
+                return base.GetUrl(context);
+            }
+        }
+    }
+}
