8a03b689c870ae3d9bf00147781c2566b57e5e30
James Newton-King <james@newtonking.com>
2018-09-13 12:35:29 +1200

    Change test parameter transformer to slugify values

--- test/WebSites/RoutingWebSite/TestParameterTransformer.cs
+++ test/WebSites/RoutingWebSite/TestParameterTransformer.cs
@@ -1,15 +1,17 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Routing;
 
 namespace RoutingWebSite
 {
-    public class TestParameterTransformer : IParameterTransformer
+    public class SlugifyParameterTransformer : IParameterTransformer
     {
         public string Transform(string value)
         {
-            return "_" + value + "_";
+            // Slugify value
+            return Regex.Replace(value, "([a-z])([A-Z])", "$1-$2").ToLower();
         }
     }
 }
