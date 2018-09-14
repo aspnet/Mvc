7319c79537c78410c869dd188b74aaf2175d2ab4
James Newton-King <james@newtonking.com>
2018-09-15 08:31:16 +1200

    Add regex timeout

--- test/WebSites/RoutingWebSite/TestParameterTransformer.cs
+++ test/WebSites/RoutingWebSite/TestParameterTransformer.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Routing;
 
@@ -11,7 +12,7 @@ namespace RoutingWebSite
         public string Transform(string value)
         {
             // Slugify value
-            return Regex.Replace(value, "([a-z])([A-Z])", "$1-$2").ToLower();
+            return Regex.Replace(value, "([a-z])([A-Z])", "$1-$2", RegexOptions.None, TimeSpan.FromMilliseconds(100)).ToLower();
         }
     }
 }
