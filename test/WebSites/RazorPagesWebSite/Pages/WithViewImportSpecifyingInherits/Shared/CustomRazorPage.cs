57752c8f12c5a0cbcae0648573df0df1d8cdbbef
Pranav K <prkrishn@hotmail.com>
2018-09-28 13:19:14 -0700

    Add tests to verify specifying @inherits in _ViewImports works with Razor Pages

---
+++ test/WebSites/RazorPagesWebSite/Pages/WithViewImportSpecifyingInherits/Shared/CustomRazorPage.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace RazorPagesWebSite
+{
+    public abstract class CustomRazorPage : RazorPage
+    {
+        public string Title
+        {
+            get => (string)ViewContext.ViewData[nameof(Title)];
+            set => ViewContext.ViewData[nameof(Title)] = value;
+        }
+    }
+}
